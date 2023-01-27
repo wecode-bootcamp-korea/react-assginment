0dd8803aa5496d5164fe02ca475f1e122a9cc458
sstaar91 <sstaar91@gmail.com>
2023-01-27 15:19:12 +0900

    ADD : 초기세팅 scss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Kimgaebal from "./Kimgaebal";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Kimgaebal />);
+root.render();
