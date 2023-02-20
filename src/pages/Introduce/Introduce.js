bd5463e7218b7dbf90a10e460906a9870aafd8c3
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-02-20 20:14:29 +0900

    Assignment 1-1 : 자기소개 컴포넌트 구현

---
+++ src/pages/Introduce/Introduce.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./Introduce.css";
+
+const Introduce = () => {
+    return (<div className = "box">
+        <div className = "wecodebox">
+            <h1>>wecode</h1>
+        </div>
+        <p>유정인</p>
+        <p>43기</p>
+    </div>
+);};
+
+export default Introduce;
