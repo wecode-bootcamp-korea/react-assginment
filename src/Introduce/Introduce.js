4fa0b89a76fdc6441fd882fb701ae5a0f514df06
ohj1su <ohsj1208@gmail.com>
2023-02-20 18:44:09 +0900

    Assignment1-1 : 자기소개 컴포넌트 구현

---
+++ src/Introduce/Introduce.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "../Introduce/Introduce.css"
+
+
+const Self = ()=> {
+    return(
+        <div className="outline">
+            <div className="logobox">
+                <h1 className="text">wecode</h1>
+            </div>
+            <h2>오지수</h2>
+            <h5>43기</h5>
+        </div>
+    ) ;
+}
+
+
+
+export default Self;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Introduce from "./Introduce/Introduce"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+root.render(<Introduce />);
