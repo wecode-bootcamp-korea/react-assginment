0c93d4efcef70f2fa966c3407557b22ba984b8bd
summer-o3o <ho_ho95@naver.com>
2023-02-20 16:53:09 +0900

    Assignment1-1 : 자기소개 컴포넌트 구현

---
+++ src/pages/Introduce/Introduce.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Introduce.scss";
+
+const Introduce = () => {
+  return (
+    <div className="wrap_introduce">
+      <div className="box_introduce">
+        <span className="thumb_introduce">&#62; wecode</span>
+        <strong className="tit_name">김개발</strong>
+        <span className="txt_introduce">오잉</span>
+      </div>
+    </div>
+  );
+};
+
+export default Introduce;
