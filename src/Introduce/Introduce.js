692739d3cdb0dd23211a977236436f70a243596e
ohj1su <ohsj1208@gmail.com>
2023-02-20 20:27:59 +0900

    Assignment1-2 : event 사용하기

--- src/Introduce/Introduce.js
+++ src/Introduce/Introduce.js
@@ -10,10 +10,22 @@ const Self = ()=> {
             </div>
             <h2>오지수</h2>
             <h5>43기</h5>
+            <form>
+                <input type="text" onChange={handleInput}></input>
+                <button type="button" onClick={handleButton}>버튼</button>
+            </form>
         </div>
     ) ;
 }
 
+const handleInput = ()=> {
+    console.log("");
+}
+
+const handleButton = ()=> {
+    console.log("");
+
+}
 
 
 export default Self;
