32babe09b4e1a40959aa5103349dbd74afcc4a2d
kitae <kitae@kitaeui-MacBookPro.local>
2023-02-20 18:45:02 +0900

    Assignment1-1 : 자기소개 컴포넌트 구현

---
+++ src/pages/Introduce/Introduce.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../Introduce/Introduce.css";
+
+const ClassName = () => {
+  return (
+    <div className="outLine">
+      <div className="logobox">
+        <h1 className="text">wecode</h1>
+      </div>
+      <p className="name"> 이기태</p>
+      <p className="th"> 43기</p>
+    </div>
+  );
+};
+
+export default ClassName;
