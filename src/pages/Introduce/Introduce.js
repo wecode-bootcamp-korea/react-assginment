d8ade5931639d074d8d03bb8cf53d1b255fe1335
frlampard <cskr1013@gmail.com>
2023-02-20 20:06:05 +0900

    Assignment1-1 : 자기소개 컴포넌트 구현

---
+++ src/pages/Introduce/Introduce.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Introduce = () => {
+    return (
+        <div className="main1">
+            <img src="https://www.three.co.uk/cs/Satellite?blobkey=id&blobwhere=1401304786181&blobcol=urldata&blobtable=MungoBlobs"/>
+            <li>
+                신건록
+            </li>
+            <li>
+                43기
+            </li>
+        </div>
+    )
+}
+
+export default Introduce;
