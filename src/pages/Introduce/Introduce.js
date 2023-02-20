cfe3866a46a842b8e0f571ede6c87a374bf63629
이이솜이 <somyi-lee@iisom-is-MacBook-Air.local>
2023-02-20 17:05:51 +0900

    Assignment1-1 : 자기소개 컴포넌트 구현

---
+++ src/pages/Introduce/Introduce.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Introduce.css";
+
+const image = () => {
+  return (
+    <div className="contentContainer">
+      <div className="logoBox">
+        <h1>&gt; wecode</h1>
+      </div>
+      <div className="nameBox">
+        <h2>김개발</h2>
+        <p>00기</p>
+      </div>
+    </div>
+  );
+};
+
+export default image;
