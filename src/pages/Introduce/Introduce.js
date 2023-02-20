d309e0585899dc531c698e2f220e82df31b96fbd
eunbinmoon <bin1109a@gmail.com>
2023-02-20 20:22:39 +0900

    Assignment1-2 : event 사용하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -9,9 +9,20 @@ const Self = () => {
           <b>문은빈</b>
         </p>
         <p>43기</p>
+        <date className="inputInfo">
+          <input type={Text} onChange={handleInput}></input>
+          <button onClick={handleButton}>클릭</button>
+        </date>
       </div>
     </div>
   );
 };
 
+function handleInput() {
+  console.log("input 태그 동작");
+}
+
+function handleButton() {
+  console.log("button 태그 동작");
+}
 export default Self;
