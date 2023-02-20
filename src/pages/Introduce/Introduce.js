8bddb8e3daf9ec1ce9697242276b887febfe4520
김민경 <gimmingyeong@gimmingyeong-ui-MacBookPro.local>
2023-02-20 20:45:29 +0900

    Assignment1-2 : event 사용하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -2,19 +2,33 @@ import React from 'react';
 import './Introduce.css';
 
 const Introduce = () => {
+  
   return (
     <div className="wrapper">
       <div className="container">
-        <div className='wecode'>
-          <p>wecode</p>
+        <div className="wecode">
+          <p>&#62;wecode</p>
         </div>
       </div>
       <div className="secContainer">
-        <p className='p'>김개발</p>
+        <strong className="p">김개발</strong>
         <p>43 기</p>
+      </div>
+      <div className="input">
+        <form>
+          <input type="text" onChange={handleInput} />
+          <button type="button" onClick={handleButton}>
+            출력
+          </button>
+        </form>
       </div>
     </div>
   );
 };
-
+const handleInput = () => {
+  console.log('');
+};
+const handleButton = () => {
+  console.log('');
+};
 export default Introduce;
