77694aec80bc160f43256fb47b8b6ffd008ae55b
kitae <kitae@kitaeui-MacBookPro.local>
2023-02-20 20:36:08 +0900

    Assignment1-2 : event 사용하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "../Introduce/Introduce.css";
 
-const ClassName = () => {
+const abcName = () => {
   return (
     <div className="outLine">
       <div className="logobox">
@@ -9,8 +9,20 @@ const ClassName = () => {
       </div>
       <p className="name"> 이기태</p>
       <p className="th"> 43기</p>
+      <form>
+        <input type="text" onChange={handleInput} />
+        <button type="button" className="login_btn" onClick={handleButton}>
+          버튼
+        </button>
+      </form>
     </div>
   );
 };
 
-export default ClassName;
+function handleInput() {
+  console.log("input 태그동작");
+}
+function handleButton() {
+  console.log("button 태그 동작");
+}
+export default abcName;
