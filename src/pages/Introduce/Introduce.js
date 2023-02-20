e21cdd4014254b838777ff6929233a8098d2d871
Yoo Jeong Kwon <kwonyj@Yooui-MacBookAir.local>
2023-02-20 18:18:55 +0900

    ADD: Assignment 1-1: 자기소개 컴포넌트 구현

---
+++ src/pages/Introduce/Introduce.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Introduce = () => {
+  return (
+    <nav className="totalWrap">
+      <div className="wecodeWrap">
+        <h1 className="wecode">> wecode</h1>
+        <p className="name">권유정</p>
+        <p className="class">43기</p>
+      </div>
+    </nav>
+  );
+};
+
+export default Introduce;
