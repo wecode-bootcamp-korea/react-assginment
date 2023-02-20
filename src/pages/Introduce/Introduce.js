1dec02a2b1bf0c96892b9ae82e5ba11aeb8b2d82
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-02-20 20:48:05 +0900

    [43기 유정인] Assignment 1-2 : event 사용하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -2,13 +2,17 @@ import React from 'react';
 import "./Introduce.css";
 
 const Introduce = () => {
-    return (<div className = "box">
+    return (
+    <div className = "box">
         <div className = "wecodebox">
             <h1>>wecode</h1>
         </div>
         <p>유정인</p>
         <p>43기</p>
+        <input type="text" onChange={(e)=>{console.log("input 태그 작동")}}></input>
+        <button onClick={(e)=>{console.log("button 태그 작동")}}>button</button>
     </div>
 );};
+
 
 export default Introduce;
