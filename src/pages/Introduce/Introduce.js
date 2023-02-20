5fb20cd1a7cef587f8c7673d6a8c999aae13e3c7
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-02-20 16:53:33 +0900

    Assignment1-1 : 자기소개 컴포넌트 구현

---
+++ src/pages/Introduce/Introduce.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Introduce.scss";
+
+const Introduce = () => {
+  return (
+    <div className="introduce">
+      <div className="introduce-box">
+        <div className="introduce-wecode">&gt;wecode</div>
+        <div className="introduce-detail">
+          <h1>장지아</h1>
+          <h1>43기</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Introduce;
