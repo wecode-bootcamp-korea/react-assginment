4b6da73438a3f5c5fc5d23ce42bf3e3e42a101e6
eunbinmoon <bin1109a@gmail.com>
2023-02-20 18:31:13 +0900

    Assignment1-1 : 자기소개 컴포넌트 구현

---
+++ src/pages/Introduce/Introduce.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./introduce.css";
+const Self = () => {
+  return (
+    <div className="main">
+      <img alt="myPicture" src="img/비니비니.jpg"></img>
+      <div className="information">
+        <p>
+          <b>문은빈</b>
+        </p>
+        <p>43기</p>
+      </div>
+    </div>
+  );
+};
+
+export default Self;
