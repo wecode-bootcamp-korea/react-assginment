ae311e7cf64327c06913dc66d7d37c2605a3a814
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-02-20 17:02:46 +0900

    Assignment1-2 : event 사용하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -9,6 +9,18 @@ const Introduce = () => {
         <div className="introduce-detail">
           <h1>장지아</h1>
           <h1>43기</h1>
+          <input
+            onChange={function handleInput() {
+              console.log("input 태그 동작");
+            }}
+          />
+          <button
+            onClick={function handleButton() {
+              console.log("button 태그 동작");
+            }}
+          >
+            클릭
+          </button>
         </div>
       </div>
     </div>
