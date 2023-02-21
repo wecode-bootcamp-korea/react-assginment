d8f425def21bfc26068056582264bf098fdf8e45
kitae <kitae@kitaeui-MacBookPro.local>
2023-02-21 17:51:16 +0900

    Assignment2-2 : Sass 적용

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../Introduce/Introduce.css";
+import "../Introduce/Introduce.scss";
 
 const Introduce = () => {
   return (
