eeb36b304829602dcd3864339724935483b58aeb
frlampard <cskr1013@gmail.com>
2023-02-22 01:25:51 +0900

    Assignment2-1 : Router 컴포넌트 구현

---
+++ src/Router.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Introduce from './pages/Introduce/Introduce';
+import Product from './pages/Product/Product';
+import ProductList from './pages/ProductList/ProductList';
+
+const Router = () => {
+    return (
+        <BrowserRouter>
+                <Routes>                    
+                    <Route path="/" element={<Introduce/>} />
+                    <Route path="./pages/Product/Product" element={<Product/>} />
+                    <Route path="/productList" element={<ProductList/>} />
+                </Routes>
+        </BrowserRouter>
+
+
+    );
+};
+
+export default Router;
--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,17 +1,33 @@
 import React from "react";
+import { Routes, Route, Link, NavLink  } from "react-router-dom";
+import Product from "../Product/Product";
+import "./Introduce.scss";
 
 const Introduce = () => {
     return (
-        <div className="main1">
-            <img src="https://www.three.co.uk/cs/Satellite?blobkey=id&blobwhere=1401304786181&blobcol=urldata&blobtable=MungoBlobs"/>
-            <li>
-                신건록
-            </li>
-            <li>
-                43기
-            </li>
+      <div className="introduce">
+        <div className="introduceBox">
+          <div><img className="introduceImg" src="https://www.edology.com/uploads/media/sulu-700x450/00/1720-blog-why-coding-is-so-important-for-IT-jobs-s.jpg?v=2-0" alt="introduceImg"/></div>
+          <div className="introduceAccount">
+            <h1>신건록</h1>
+            <h1>43기</h1>
+            <input onChange={function handleInput() {
+                console.log("input 동작 ㅇㅈ");
+              }}
+            />
+            <button className="Product" onClick={function handleButton() {
+                console.log("button 동작 ㅇㅈ");
+              }}
+            >클릭</button>
+            <li><Link to="../Product/Product">aaa</Link></li>
+            <Routes>
+              <Route path="../Product/Product" element={<Product/>}></Route>
+            </Routes>
+            <NavLink to="../Product/Product">product로 이동</NavLink>
+          </div>
         </div>
-    )
-}
+      </div>
+      )
+};
 
 export default Introduce;
