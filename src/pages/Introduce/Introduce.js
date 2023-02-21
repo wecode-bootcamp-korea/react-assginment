e7c0fca6fe819a545673434475a0c24887cb7cc8
eunbinmoon <bin1109a@gmail.com>
2023-02-21 16:59:53 +0900

    Assignment2-1 : Router 컴포넌트 구현

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,6 +1,10 @@
 import React from "react";
-import styles from "./introduce.css";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import "./introduce.css";
+
 const Self = () => {
+  const navigate = useNavigate();
   return (
     <div className="main">
       <img alt="myPicture" src="img/비니비니.jpg"></img>
@@ -10,19 +14,20 @@ const Self = () => {
         </p>
         <p>43기</p>
         <date className="inputInfo">
-          <input type={Text} onChange={handleInput}></input>
-          <button onClick={handleButton}>클릭</button>
+          <input type={Text}></input>
+          <button onClick={() => navigate("/product")}>클릭</button>
         </date>
+        <Link to="/product">Product 페이지로 이동!</Link>
       </div>
     </div>
   );
 };
 
-function handleInput() {
-  console.log("input 태그 동작");
-}
+// function handleInput() {
+//   console.log("input 태그 동작");
+// }
 
-function handleButton() {
-  console.log("button 태그 동작");
-}
+// function handleButton() {
+//   console.log("button 태그 동작");
+// }
 export default Self;
