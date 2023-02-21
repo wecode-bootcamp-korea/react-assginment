180339c488335bfbf17f5f92727cb5887bcbbfcd
Dante6327 <jtgk6327@hanmail.net>
2023-02-21 16:22:12 +0900

    Assignment2-2 : Sass 적용

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,45 +1,6 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-
-const IntroduceStyle = {
-  border: "1px solid black",
-  width: "110px",
-  borderRadius: "5px",
-};
-
-const mainStyle = {
-  margin: "8px",
-  backgroundColor: "black",
-  borderRadius: "5px",
-};
-
-const contentStyle = {
-  display: "flex",
-  justifyContent: "center",
-  alignItems: "center",
-  color: "white",
-  height: "100px",
-};
-
-const userStyle = {
-  margin: "0px 0px 5px 10px",
-  fontSize: "10px",
-};
-
-const input = {
-  width: "55%",
-  fontSize: "8px",
-};
-
-const button = {
-  padding: "0",
-  width: "30px",
-  fontSize: "8px",
-};
-
-const linkToProduct = {
-  fontSize: "8px",
-};
+import "./Introduce.scss";
 
 const Introduce = () => {
   const navigate = useNavigate();
@@ -53,28 +14,21 @@ const Introduce = () => {
   };
 
   return (
-    <div className="Introduce" style={IntroduceStyle}>
-      <section className="main" style={mainStyle}>
-        <div className="content" style={contentStyle}>
-          ＞wecode
-        </div>
+    <div className="Introduce">
+      <section className="main">
+        <div className="content">＞wecode</div>
       </section>
       <section className="userInfo">
-        <div className="user" style={userStyle}>
-          <b>김진평</b>
+        <div className="user">
+          <b className="userName">김진평</b>
           <br />
           43기
           <br />
-          <input
-            type="text"
-            className="inputText"
-            style={input}
-            onChange={handleChange}
-          />
-          <button className="button" style={button} onClick={handleClick}>
+          <input type="text" className="inputText" onChange={handleChange} />
+          <button className="button" onClick={handleClick}>
             클릭
           </button>
-          <Link to="/product" style={linkToProduct}>
+          <Link to="/product" className="linkToProduct">
             product 페이지로 이동
           </Link>
         </div>
