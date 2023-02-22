e6117621c5e284a59c771272a2700dd0af1719d2
eunbinmoon <bin1109a@gmail.com>
2023-02-22 21:40:46 +0900

    프로젝트 초기세팅

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./introduce.scss";
 
 const Self = () => {
   const navigate = useNavigate();
+
+  const handleButton = () => {
+    navigate("/product");
+  };
+
   return (
     <div className="main">
       <img alt="myPicture" src="img/비니비니.jpg"></img>
@@ -13,7 +17,7 @@ const Self = () => {
         <p>43기</p>
         <date className="inputInfo">
           <input type={Text}></input>
-          <button onClick={() => navigate("/product")}>클릭</button>
+          <button onClick={handleButton}>클릭</button>
         </date>
         <Link to="/product">Product 페이지로 이동!</Link>
       </div>
