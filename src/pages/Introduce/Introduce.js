85690501d3489518473b5b81ea9479c0c938371b
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-02-22 21:08:13 +0900

    MODIFY : Assignment 2-2 피드백 반영

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,31 +1,31 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import "./Introduce.scss";
 import "../../style/variables.scss";
 import "../../style/mixin.scss";
 
 const Introduce = () => {
+  const navigate = useNavigate();
+
+  const goToProduct = () => {
+    navigate("/product");
+  };
+
+  const handleInput = () => {
+    console.log("input 태그 동작");
+  };
+
   return (
     <div className="introduce">
       <div className="introduce-box">
         <div className="introduce-wecode">&gt;wecode</div>
         <div className="introduce-detail">
-          <h1>장지아</h1>
-          <h1>43기</h1>
+          <h2>장지아</h2>
+          <h2>43기</h2>
           <div className="input-box">
-            <input
-              onChange={function handleInput() {
-                console.log("input 태그 동작");
-              }}
-            />
-            <button
-              onClick={() => {
-                window.location.href = "/product";
-              }}
-            >
-              클릭
-            </button>
+            <input onChange={handleInput} />
+            <button onClick={goToProduct}>클릭</button>
           </div>
           <Link to="/product">product 페이지로 이동</Link>
         </div>
