4775bd28098464206a153c8fd15af0cd04a369bd
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-02-22 11:35:45 +0900

    ADD : 현재까지 진행사항

--- src/Router.js
+++ src/Router.js
@@ -4,9 +4,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Introduce from "./pages/Introduce/Introduce";
 import Product from "./pages/Product/Product";
 import ProductList from "./pages/ProductList/ProductList";
-// import Review from "./pages/Review/Review";
-// import Count from "./pages/Count/Count";
-// import Color from "./pages/Color/Color";
+import Review from "./pages/Product/components/Review/Review";
+import Count from "./pages/Product/components/Count/Count";
+import Color from "./pages/Product/components/Color/Color";
 
 const Router = () => {
   return (
@@ -15,9 +15,9 @@ const Router = () => {
         <Route path="/" element={<Introduce />} />
         <Route path="/product" element={<Product />} />
         <Route path="/productList" element={<ProductList />} />
-        {/* <Route path="/review" element={<Review />} />
+        <Route path="/review" element={<Review />} />
         <Route path="/count" element={<Count />} />
-        <Route path="/color" element={<Color />} /> */}
+        <Route path="/color" element={<Color />} />
         <Route path="*" element={<div>해당 주소는 없는 페이지입니다.</div>} />
       </Routes>
     </BrowserRouter>
--- src/pages/Product/components/Count/Count.js
+++ src/pages/Product/components/Count/Count.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Count.scss";
 
 const Count = () => {
+  const [num, setNum] = useState("1");
+
   return (
     <div className="count">
       <div className="countInput">
-        <button onClick={() => {}}>-</button>
-        <div className="countInputText">1</div>
-        <button onClick={() => {}}>+</button>
+        <button
+          onClick={() => {
+            num > 1 ? setNum(Number(num) - 1) : setNum("1");
+          }}
+        >
+          -
+        </button>
+        <div className="countInputText">{num}</div>
+        <button
+          onClick={() => {
+            setNum(Number(num) + 1);
+          }}
+        >
+          +
+        </button>
       </div>
-      <button className="resetBtn" onClick={() => {}}>
+      <button
+        className="resetBtn"
+        onClick={() => {
+          setNum("1");
+        }}
+      >
         reset
       </button>
     </div>
