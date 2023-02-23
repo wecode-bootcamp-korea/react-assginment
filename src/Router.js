2bd1d6a6ebea55c01744abcfdb7c16611da894d2
김민경 <gimmingyeong@gimmingyeong-ui-MacBookPro.local>
2023-02-23 21:42:25 +0900

    Assignment3-3 : Review 컴포넌트 구현

--- src/Router.js
+++ src/Router.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Introduce from './pages/Introduce/Introduce';
 import Product from './pages/Product/Product';
 import ProductList from './pages/ProductList/ProductList';
-import Review from './pages/ProductList/ProductList';
+import Review from './pages/Product/components/Review/Review';
 import Count from './pages/Product/components/Count/Count';
 import Color from './pages/Product/components/Color/Color';
 
--- src/pages/Product/components/Review/Review.js
+++ src/pages/Product/components/Review/Review.js
@@ -1,7 +1,9 @@
-import React from "react";
-import "./Review.scss";
+import React, { useState } from 'react';
+import './Review.scss';
 
 const Review = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <div className="review">
       <div className="user">
@@ -13,13 +15,18 @@ const Review = () => {
       </div>
       <div className="reviewTitle">배송도 빠르고 정말 좋아요.</div>
       <div className="reviewMain">
-        <div className="reviewMainContents">
+        <div className={`reviewMainContents ${isOpen ? 'show' : 'block'}`}>
           주문한지 하루만에 배송이 도착해서 너무 좋았습니다.
           <br />
           다음에도 필요하면 또 여기서 시킬 것 같아요.
         </div>
-        <span className="reviewMainHandler" onClick={() => {}}>
-          더보기 ▼
+        <span
+          className="reviewMainHandler"
+          onClick={() => {
+            setIsOpen((isOpen) => !isOpen);
+          }}
+        >
+          {isOpen ? '접기 ▼' : '더보기'}
         </span>
       </div>
     </div>
