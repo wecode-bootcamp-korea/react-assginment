d7411125e9172ec146b590009582312675238ef4
frlampard <cskr1013@gmail.com>
2023-02-23 21:41:22 +0900

    Assignment3-1 : Color 컴포넌트 구현

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Introduce from './pages/Introduce/Introduce';
 import Product from './pages/Product/Product';
 import ProductList from './pages/ProductList/ProductList';
+import Color from './pages/Product/components/Color/Color';
 
 const Router = () => {
     return (
@@ -11,6 +12,7 @@ const Router = () => {
                     <Route path="/" element={<Introduce/>} />
                     <Route path="/product" element={<Product/>} />
                     <Route path="/productList" element={<ProductList/>} />
+                    <Route path="/color" element={<Color/>} />
                 </Routes>
         </BrowserRouter>
 
