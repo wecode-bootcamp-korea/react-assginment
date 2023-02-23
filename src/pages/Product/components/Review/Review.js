c3205252080e1d313d67830b7c4af9063b4121e1
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-02-23 17:41:39 +0900

    Assignment3-3 : Review 컴포넌트 구현

--- src/pages/Product/components/Review/Review.js
+++ src/pages/Product/components/Review/Review.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Review.scss";
 
 const Review = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleToggle = () => {
+    setIsOpen(isOpen => !isOpen);
+  }
+
   return (
     <div className="review">
       <div className="user">
@@ -12,15 +17,16 @@ const Review = () => {
         </div>
       </div>
       <div className="reviewTitle">배송도 빠르고 정말 좋아요.</div>
-      <div className="reviewMain">
+      <div className="reviewMain">{isOpen && (
         <div className="reviewMainContents">
           주문한지 하루만에 배송이 도착해서 너무 좋았습니다.
           <br />
           다음에도 필요하면 또 여기서 시킬 것 같아요.
-        </div>
-        <span className="reviewMainHandler" onClick={() => {}}>
-          더보기 ▼
+        </div>)}
+        <span className="reviewMainHandler" onClick={handleToggle}>
+        {isOpen ? "접기 ▲ " : "더보기 ▼"}
         </span>
+        <div className='hide'></div>
       </div>
     </div>
   );
