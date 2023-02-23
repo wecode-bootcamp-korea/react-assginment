bc3ecbc20ca1280e1203b8a146f299814c5e7d98
summer-o3o <ho_ho95@naver.com>
2023-02-23 11:24:51 +0900

    REFACTORING : 리뷰 반영

--- src/Router.js
+++ src/Router.js
@@ -1,16 +1,34 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Product from './pages/Product/Product';
-import ProductList from './pages/ProductList/ProductList';
+
+// Introduce
 import Introduce from './pages/Introduce/Introduce';
+
+// ProductList
+import ProductList from './pages/ProductList/ProductList';
+
+// Product
+import Product from './pages/Product/Product';
+import Color from './pages/Product/components/Color/Color';
+import Review from './pages/Product/components/Review/Review';
+import Count from './pages/Product/components/Count/Count';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="/" element={<Introduce />} />
-        <Route path="/product" element={<Product />} />
+
+        {/* Introduce */}
+        <Route path="/" element={<Introduce />} />
+
+         {/* ProductList */}
         <Route path="/productList" element={<ProductList />} />
+
+         {/* Product */}
+        <Route path="/product" element={<Product />} />
+        <Route path="/color" element={<Color />} />
+        <Route path="/review" element={<Review />} />
+        <Route path="/count" element={<Count />} />
       </Routes>
     </BrowserRouter>
  );
--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,8 +1,7 @@
 import React from "react";
+import { Link, useNavigate } from 'react-router-dom';
 import "./Introduce.scss";
 import "../../style/mixin.scss";
-import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
 
 const Introduce = () => {
   const navigate = useNavigate();
@@ -14,11 +13,6 @@ const Introduce = () => {
   function handleInput() {
     console.log('input 태그 동작');
   }
-
-  function handleButton() {
-    console.log('button 태그 동작');
-  }
-
   return (
     <div className="wrap_introduce">
       <div className="box_introduce">
