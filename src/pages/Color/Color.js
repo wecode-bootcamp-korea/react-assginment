47658fa1178327c390a6562dd01e5ad4ccb4d961
Dante6327 <jtgk6327@hanmail.net>
2023-02-23 15:49:19 +0900

    Assignment3-1 : Color 컴포넌트 구현

--- src/pages/Color/Color.js
+++ src/pages/Color/Color.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Color.scss";
 
 const Color = () => {
-  return <div>Color</div>;
+  const [color, setColor] = useState("White");
+
+  return (
+    <div className="color">
+      <b>
+        색상 : <div className={`colorResult color${color}`} /> {color}
+      </b>
+      <div className="selectColor">
+        <div
+          onClick={() => setColor("White")}
+          className={`colorBox colorWhite`}
+        />
+        <div onClick={() => setColor("Red")} className={`colorBox colorRed`} />
+        <div
+          onClick={() => setColor("Yellow")}
+          className={`colorBox colorYellow`}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Color;
