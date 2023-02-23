35d7854f03807be6e32848147cc919676ca77e87
zunyange <hiillun00553300@gmail.com>
2023-02-23 20:51:38 +0900

    Assignment3-3 : Review 컴포넌트 구현

--- src/pages/Product/components/Color/Color.js
+++ src/pages/Product/components/Color/Color.js
@@ -1,6 +1,6 @@
 import React from "react";
-import "./Color.scss";
 import { useState } from "react";
+import "./Color.scss";
 
 const Color = () => {
   const [color, setColor] = useState("white");
