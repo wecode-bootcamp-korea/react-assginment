0a9278c8d117811d85a45dfd0100915760403dac
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-02-23 16:25:31 +0900

    Assignment3-3 : Review 컴포넌트 구현

--- src/pages/Product/components/Review/Review.js
+++ src/pages/Product/components/Review/Review.js
@@ -2,19 +2,8 @@ import React, { useState } from "react";
 import "./Review.scss";
 
 const Review = () => {
+  const [isOpen, setIsOpen] = useState(true);
   const [toggle, setToggle] = useState("더보기 ▼");
-  const [display, setDisplay] = useState("none");
-
-  const handleToggle = () => {
-    setToggle("접기 ▲");
-    setDisplay("block");
-  };
-
-  // const [isOpen, setMenu] = useState(false); // 메뉴의 초기값을 false로 설정
-
-  // const toggleMenu = () => {
-  //   setMenu((isOpen) => !isOpen); // on,off 개념 boolean
-  // };
 
   return (
     <div className="review">
@@ -27,12 +16,18 @@ const Review = () => {
       </div>
       <div className="reviewTitle">배송도 빠르고 정말 좋아요.</div>
       <div className="reviewMain">
-        <div className="reviewMainContents" style={{ display: display }}>
+        <div className={`reviewMainContents ${isOpen ? "hidden" : "open"}`}>
           주문한지 하루만에 배송이 도착해서 너무 좋았습니다.
           <br />
           다음에도 필요하면 또 여기서 시킬 것 같아요.
         </div>
-        <span className="reviewMainHandler" onClick={handleToggle}>
+        <span
+          className="reviewMainHandler"
+          onClick={() => {
+            setIsOpen(!isOpen);
+            isOpen ? setToggle("접기 ▲") : setToggle("더보기 ▼");
+          }}
+        >
           {toggle}
         </span>
       </div>
