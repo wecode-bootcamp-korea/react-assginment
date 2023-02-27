e86f66adc8ff47c625af7561e3536c4eddc58fd4
frlampard <cskr1013@gmail.com>
2023-02-27 16:12:47 +0900

    Assignment3-5 : Product 컴포넌트 구현하기

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Product.scss";
 import ColorButton from './components/ColorButton/ColorButton';
 import Color from "./components/Color/Color";
@@ -6,15 +6,19 @@ import Count from "./components/Count/Count";
 import Review from "./components/Review/Review";
 
 const Product = () => {
+  const [count, setCount] = useState(1);
+  const [color, setColor] = useState("white");
+
   const price = 300;
-  const totalPrice = price;
+  //const totalPrice = price * `${count}`;
+  const totalPrice = price * count;
 
   return (
     <div className="product">
       <div className="productDetail">
         <div className="productDetailImg">
           <img
-            src={`./../public/images/golf-ball-.jpg`} // color 이름에 따라 다른 이미지 경로 넣기
+            src={`/images/golf-ball-${color}.jpg`} // color 이름에 따라 다른 이미지 경로 넣기
             alt={`golf-ball`}
           />
           <ColorButton />
@@ -26,7 +30,7 @@ const Product = () => {
           <Color />
           <div className="quantity">
             <span> 수량 : </span>
-          <Count />
+          <Count count={count} setCount={setCount} />
           </div>
           <span>최종 가격 : {totalPrice.toLocaleString()} 원</span>
           <button className="buyBtn">구매하기</button>
--- src/pages/Product/components/Count/Count.js
+++ src/pages/Product/components/Count/Count.js
@@ -1,31 +1,31 @@
 import React, { useState } from "react";
 import "./Count.scss";
 
-const Count = () => {
-  const [count, setCount] = useState(1);
+const Count = (props) => {
+  // const [count, setCount] = useState(1);
 
   const minusCount = () => {
     // if (count === 1 && num === -1) return;
-    if (count > 1) {
-      setCount(count - 1);
+    if (props.count > 1) {
+      props.setCount(props.count - 1);
     } else {
-      setCount(1);
+      props.setCount(1);
     }
     
   }
 
   const plusCount = () => {
-    setCount(count + 1);
+    props.setCount(props.count + 1); 
   }
   
   return (
     <div className="count">
       <div className="countInput">
         <button onClick={minusCount}>-</button>
-        <div className={`countInputText ${count}`}>{count}</div>
+        <div className={`countInputText ${props.count}`}>{props.count}</div>
         <button onClick={plusCount}>+</button>
       </div>
-      <button className="resetBtn" onClick={() => {setCount(1)}}>
+      <button className="resetBtn" onClick={() => {props.setCount(1)}}>
         reset
       </button>
     </div>
