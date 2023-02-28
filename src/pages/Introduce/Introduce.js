60fa193597828333ba32673c8c9b692b4d04ad87
Jaylen Yu <114912854+jaylenyu@users.noreply.github.com>
2023-02-28 23:08:33 +0900

    Assignment4-2 : ColorCard 상수데이터로 구현하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { useNavigate, Link } from 'react-router-dom';
-import Nav from '../components/Nav/Nav';
 import "./Introduce.scss";
-
 
 const Introduce = () => {
 
@@ -19,17 +17,34 @@ const Introduce = () => {
     return (
     <div className = "introduce">  
         <div className = "wecodebox">
+            <div className="wecodeboxColor">
             <h1>>wecode</h1>
-        </div>
-        <p>유정인</p>
-        <p>43기</p>
-        <div>
+            </div>
+            <h2>유정인</h2>
+            <h3>43기</h3>
+        <div className="wecodeboxInputButton">
             <input type="text" onChange={handleInput}/>
             <button onClick={goToProduct}>button</button>
         </div>
-        <span>
             <Link to="/product">product 페이지로 이동</Link>
-        </span>
+        </div>
+        <div className="miniBoxContainer">
+            {COLOR_CARD_LIST.map((info) => (
+                <div key={info.id} className="miniBox">
+                    <div className="miniBoxColor" style={{backgroundColor:`${info.color}`}}>
+                    </div>
+                    <h2>이름 : {info.name}</h2>
+                    <h3>rgb : {info.rgb}</h3> 
+                </div> 
+            ))}
+        </div>
     </div>
 );};
  export default Introduce;
+
+const COLOR_CARD_LIST = [
+    {id : 1, color: 'red', name : 'red', rgb : '255, 0, 0'},
+    {id : 2, color: 'blue',name : 'blue', rgb : '255, 255, 0'},    
+    {id : 3, color: 'yellow',name : 'yellow', rgb : '255, 0, 255'},    
+    {id : 4, color: 'green',name : 'green', rgb : '0, 255, 0'}
+]
