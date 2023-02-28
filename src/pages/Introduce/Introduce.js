50c723f0234c0ddd3c26a6fc9ea78f48721dbe8d
Yoo Jeong Kwon <kwonyj@Yooui-MacBookAir.local>
2023-02-28 20:39:50 +0900

    ADD: Assignment4-2 : ColorCard 상수데이터로 구현하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -26,8 +26,26 @@ const Introduce = () => {
           <p className="moveToProduct">product 페이지로 이동</p>
         </Link>
       </div>
+      <div className="colorCardWrap">
+        {CARD_LIST.map((cardInfo) => {
+          return (
+            <div key={cardInfo.id} className="wecodeWrap">
+              <h1 className={`wecode ${cardInfo.name}`}></h1>
+              <p className="name">{`이름 : ${cardInfo.name}`}</p>
+              <p className="class">{`rgb: ${cardInfo.rgb}`}</p>
+            </div>
+          );
+        })}
+      </div>
     </nav>
   );
 };
 
 export default Introduce;
+
+const CARD_LIST = [
+  { id: 1, name: "red", rgb: "255,0,0" },
+  { id: 2, name: "yellow", rgb: "255,255,0" },
+  { id: 3, name: "green", rgb: "0,128,0" },
+  { id: 4, name: "blue", rgb: "0,0,255" },
+];
