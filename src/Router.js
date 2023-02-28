a9d70d481cf6fdc119977dd1786b66157f2137a0
summer-o3o <ho_ho95@naver.com>
2023-02-28 17:49:07 +0900

    Add : Nav 작업중 임시 커밋

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+// Common
+import Nav from "./pages/components/Nav/Nav"
+
 // Introduce
 import Introduce from './pages/Introduce/Introduce';
 
@@ -16,6 +19,7 @@ import Count from './pages/Product/components/Count/Count';
 const Router = () => {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
 
         {/* Introduce */}
