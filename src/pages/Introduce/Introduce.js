4eab2984fdc2cf8b98f396a619d32549c33c77a2
kitae <kitae@kitaeui-MacBookPro.local>
2023-02-28 23:41:37 +0900

    [Assignment 4-2] React Data 활용 : 상수데이터로 ColorCard 구현하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -5,7 +5,7 @@ import "../Introduce/Introduce.scss";
 const Introduce = () => {
   const navigate = useNavigate();
 
-  const goToProduct = () => {
+  const handleButton = () => {
     navigate("/product");
   };
 
@@ -14,25 +14,48 @@ const Introduce = () => {
   }
 
   return (
-    <div className="introduce">
-      <div className="logobox">
-        <h1 className="text">wecode</h1>
+    <>
+      <div className="introduce">
+        <div className="mainBox">
+          <div className="logoBox">
+            <h1>&gt; wecode</h1>
+          </div>
+          <div className="nameBox">
+            <h2>이기태</h2>
+            <p>43기</p>
+          </div>
+          <form>
+            <input type="text" onChange={handleInput}></input>
+            <button type="button" onClick={handleButton}>
+              클릭
+            </button>
+          </form>
+          <Link to="/product">Product 페이지로 이동</Link>
+        </div>
+        <div className="colorBoxContainter">
+          {CARD_LIST.map((info) => (
+            <div className="colorBox">
+              <div
+                className="logoBox"
+                style={{ backgroundColor: `${info.color}` }}
+              ></div>
+              <div className="nameBox">
+                <h2>이름 : {info.color}</h2>
+                <p>rgb : {info.rgb}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-      <p className="name"> 이기태</p>
-      <p className="th"> 43기</p>
-      <form>
-        <input type="text" onChange={handleInput} />
-
-        <button type="button" className="login_btn" onClick={goToProduct}>
-          클릭
-        </button>
-      </form>
-
-      <p className="moveToProduct">
-        <Link to="/product">Product 페이지로 이동</Link>
-      </p>
-    </div>
+    </>
   );
 };
 
 export default Introduce;
+
+const CARD_LIST = [
+  { id: 1, color: "red", rgb: "255, 0, 0" },
+  { id: 2, color: "yellow", rgb: "255, 255, 0" },
+  { id: 3, color: "green", rgb: "0, 128, 0" },
+  { id: 4, color: "blue", rgb: "0, 0, 255" },
+];
