8a18a43b0f4b4b77a5383807d6cfd329861c9d45
HongTaeHoon <kdn95374888@gmail.com>
2023-02-28 21:33:36 +0900

    Assignment4-2 : ColorCard 상수데이터로 구현하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -14,28 +14,60 @@ const Introduce = () => {
   }
 
   return (
-    <div className="introduce">
-      <div className="boxWwrap">
-        <div className="weCodeBox">
-          <h2>{">"}wecode</h2>
+    <>
+      <div className="introduce">
+        <div className="boxWwrap">
+          <div className="weCodeBox">
+            <h2>{">"}wecode</h2>
+          </div>
+        </div>
+        <strong className="info">홍태훈</strong>
+        <div className="info">43기</div>
+        <div className="inputBox">
+          <input className="input" onChange={handleInput}></input>
+
+          <button className="button" onClick={handleButton}>
+            클릭
+          </button>
+        </div>
+        <div>
+          <Link to="/product" className="link">
+            product페이지로 이동
+          </Link>
         </div>
       </div>
-      <strong className="info">홍태훈</strong>
-      <div className="info">43기</div>
-      <div className="inputBox">
-        <input className="input" onChange={handleInput}></input>
-
-        <button className="button" onClick={handleButton}>
-          클릭
-        </button>
+      <div className="flexBox">
+        {COLOR_CARD.map((info) => {
+          return (
+            <div key={info.id} className="colorBox">
+              <div className="boxWwrap ">
+                <div className={`weCodeBox ${info.color} `} />
+              </div>
+              <strong className="info">{info.name}</strong>
+              <div className="info">{info.colorValue}</div>
+            </div>
+          );
+        })}
       </div>
-      <div>
-        <Link to="/product" className="link">
-          product페이지로 이동
-        </Link>
-      </div>
-    </div>
+    </>
   );
 };
 
 export default Introduce;
+
+const COLOR_CARD = [
+  { id: 1, color: "red", name: "이름 : red", colorValue: "rgb : 255, 0, 0" },
+  {
+    id: 2,
+    color: "yellow",
+    name: "이름 : yellow",
+    colorValue: "rgb : 255, 255, 0",
+  },
+  {
+    id: 3,
+    color: "green",
+    name: "이름 : green",
+    colorValue: "rgb : 0, 128, 0",
+  },
+  { id: 4, color: "blue", name: "이름 : blue", colorValue: "rgb : 0, 0, 255" },
+];
