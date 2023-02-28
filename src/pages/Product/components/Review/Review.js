61c86c4ce955eec86921436fcb2d13b10be55155
Jia <71865277+jangjia01234@users.noreply.github.com>
2023-02-28 17:49:39 +0900

    MODIFY : 현재까지 진행사항 커밋

--- src/pages/Product/components/Review/Review.js
+++ src/pages/Product/components/Review/Review.js
@@ -2,8 +2,8 @@ import React, { useState } from "react";
 import "./Review.scss";
 
 const Review = () => {
-  const [isOpen, setIsOpen] = useState(true);
-  const [toggle, setToggle] = useState("더보기 ▼");
+  const [isOpen, setIsOpen] = useState(false);
+  const [toggleText, setToggleText] = useState("더보기 ▼");
 
   return (
     <div className="review">
@@ -16,7 +16,7 @@ const Review = () => {
       </div>
       <div className="reviewTitle">배송도 빠르고 정말 좋아요.</div>
       <div className="reviewMain">
-        <div className={`reviewMainContents ${isOpen ? "hidden" : "open"}`}>
+        <div className={`reviewMainContents ${isOpen ? "open" : "hidden"}`}>
           주문한지 하루만에 배송이 도착해서 너무 좋았습니다.
           <br />
           다음에도 필요하면 또 여기서 시킬 것 같아요.
@@ -25,10 +25,10 @@ const Review = () => {
           className="reviewMainHandler"
           onClick={() => {
             setIsOpen(!isOpen);
-            isOpen ? setToggle("접기 ▲") : setToggle("더보기 ▼");
+            isOpen ? setToggleText("더보기 ▼") : setToggleText("접기 ▲");
           }}
         >
-          {toggle}
+          {toggleText}
         </span>
       </div>
     </div>
