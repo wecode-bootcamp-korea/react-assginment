e9201cd3be5d8ee8dfbbe74c71b5b123c2b1ec72
Dante6327 <jtgk6327@hanmail.net>
2023-02-28 19:59:54 +0900

    Assignment4-2 : ColorCard 상수데이터로 구현하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -4,37 +4,57 @@ import "./Introduce.scss";
 
 const Introduce = () => {
   const navigate = useNavigate();
-  const handleChange = () => {
-    console.log("input 태그 동작");
-  };
+  const handleChange = () => {};
 
   const handleClick = () => {
-    console.log("button 태그 동작");
     navigate("/product");
   };
 
   return (
     <div className="introduce">
-      <section className="main">
-        <div className="content">＞wecode</div>
-      </section>
-      <section className="userInfo">
-        <div className="user">
-          <b className="userName">김진평</b>
-          <br />
-          43기
-          <br />
-          <input type="text" className="inputText" onChange={handleChange} />
-          <button className="button" onClick={handleClick}>
-            클릭
-          </button>
-          <Link to="/product" className="linkToProduct">
-            product 페이지로 이동
-          </Link>
+      <div className="introContainer">
+        <section className="main">
+          <div className="content">＞wecode</div>
+        </section>
+        <section className="userInfo">
+          <div className="user">
+            <b className="userName">김진평</b>
+            <br />
+            43기
+            <br />
+            <input type="text" className="inputText" onChange={handleChange} />
+            <button className="button" onClick={handleClick}>
+              클릭
+            </button>
+            <Link to="/product" className="linkToProduct">
+              product 페이지로 이동
+            </Link>
+          </div>
+        </section>
+      </div>
+      {COLOR_LIST.map((color) => (
+        <div className="introContainer color">
+          <section className={`main ${color.name}`}>
+            <div className="content"></div>
+          </section>
+          <section className="userInfo">
+            <div className="user">
+              <b className="userName">이름 : {color.name}</b>
+              <br />
+              <b>rgb : {color.rgb}</b>
+            </div>
+          </section>
         </div>
-      </section>
+      ))}
     </div>
   );
 };
 
 export default Introduce;
+
+const COLOR_LIST = [
+  { id: 2, rgb: "255, 0, 0", name: "red" },
+  { id: 3, rgb: "255, 255, 0", name: "yellow" },
+  { id: 4, rgb: "0, 128, 0", name: "green" },
+  { id: 5, rgb: "0, 0, 255", name: "blue" },
+];
