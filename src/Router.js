df0c5c9a6148ab5798ac7f36117941235deb2947
ohj1su <ohsj1208@gmail.com>
2023-02-28 20:53:56 +0900

    Assignment4-1 : menu List 상수데이터로 구현

--- src/Router.js
+++ src/Router.js
@@ -7,20 +7,22 @@ import Review from "./pages/Product/components/Review/Review";
 import Count from "./pages/Product/components/Count/Count";
 import Color from "./pages/Product/components/Color/Color";
 import ColorButton from "./pages/Product/components/ColorButton/ColorButton";
+import Nav from "./pages/components/Nav/Nav";
 
 
 
 const Router = () => {
     return(
         <BrowserRouter>
+                    {<Nav/>}
         <Routes>
             <Route path ="/" element = {<Introduce/>}/>
-            <Route path ="/Product" element = {<Product/>}/>
-            <Route path ="/ProductList" element = {<ProductList/>}/>
-            <Route path ="/Review" element = {<Review/>}/>
-            <Route path ="/Count" element = {<Count/>}/>
-            <Route path ="/Color" element = {<Color/>}/>
-            <Route path ="/ColorButton" element = {<ColorButton/>}/>
+            <Route path ="/product" element = {<Product/>}/>
+            <Route path ="/productList" element = {<ProductList/>}/>
+            <Route path ="/review" element = {<Review/>}/>
+            <Route path ="/count" element = {<Count/>}/>
+            <Route path ="/color" element = {<Color/>}/>
+            <Route path ="/colorButton" element = {<ColorButton/>}/>
         </Routes>
         </BrowserRouter>
     )
