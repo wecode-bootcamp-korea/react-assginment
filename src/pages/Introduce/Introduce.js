3c306f595fa3ae1d36de516de84ce89f5b9d86b5
zunyange <hiillun00553300@gmail.com>
2023-02-28 21:41:35 +0900

    Assignment4-2 : ColorCard 상수데이터로 구현하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,17 +1,17 @@
 import React from "react";
 import "../../pages/Introduce/Introduce.scss";
 
-import { Link } from "react-router-dom";
+import { Link, redirect } from "react-router-dom";
 
 const Introduce = () => {
   //첫 부모태그 className은 컴포넌트이름을 그대로 사용하되, 소문자로 하는게 좋음 box 말고 introduce
   return (
     <div className="introduce">
       <div className="weCodeBox">
-        <div className="wecodeStyle">{">"}wecode</div>
+        <div className="weCodeStyle">{">"}wecode</div>
         <div className="infoWrap">
           <strong className="content">김준영</strong>
-          <div className="subcontent">43기</div>
+          <div className="subContent">43기</div>
           <input
             className="input"
             type="text"
@@ -34,8 +34,27 @@ const Introduce = () => {
           </div>
         </div>
       </div>
+      <div className="colorList">
+        {COLOR_LIST.map((info) => (
+          <div key={info.id} className="colorBox">
+            <div
+              className="colorListColor"
+              style={{ backgroundColor: `${info.name}` }}
+            ></div>
+            <div className="colorName">이름 : {info.name}</div>
+            <div className="colorRgbName">rgb : {info.rgb}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
 
 export default Introduce;
+
+const COLOR_LIST = [
+  { id: 1, name: "red", rgb: "255, 0, 0" },
+  { id: 2, name: "yellow", rgb: "255, 255, 0" },
+  { id: 3, name: "green", rgb: "0, 128, 0" },
+  { id: 4, name: "blue", rgb: "0, 0, 255" },
+];
