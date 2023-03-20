63b903731177e98fdd1d2cdbb1605cdc473e5cf7
조건호 <geonho@jogeonhoui-MacBookPro.local>
2023-03-20 20:04:10 +0900

    Assignment1-2 : event 사용하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,11 +1,32 @@
 import React from "react";
+
 import "./Introduce.scss";
 
 const Introduce = () => {
+  //const [inputValue, setInputValue] = usestate(" ");
+
+  const HandleInput = () => {
+    console.log("input태그 동작");
+  };
+
+  const HandleButton = (e) => {
+    e.preventDefault();
+    console.log("button태그 동작");
+  };
+
   return (
     <div className="box">
       <div className="image"></div>
       <p>44기 조건호</p>
+      <form>
+        <input
+          type="text"
+          onChange={(e) => {
+            HandleInput(e);
+          }}
+        />
+        <button onClick={HandleButton}>클릭</button>
+      </form>
     </div>
   );
 };
