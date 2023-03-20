d6bcb2962c4984b52764089f294e61bb4135171d
조건호 <geonho@jogeonhoui-MacBookPro.local>
2023-03-20 17:08:08 +0900

    Assignment1-1 : 자기소개 컴포넌트 구현

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
+import Introduce from "./pages/Introduce/Introduce";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+root.render(<Introduce />);
---
+++ src/pages/Introduce/Introduce.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Introduce.scss";
+
+const Introduce = () => {
+  return (
+    <div className="box">
+      <div className="image"></div>
+      <p>44기 조건호</p>
+    </div>
+  );
+};
+
+export default Introduce;
