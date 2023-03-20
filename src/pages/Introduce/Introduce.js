3162d119715e1188ff8e29a042bf6142449d4d75
신효민 <beanphone@sinhyomin-ui-MacBookPro.local>
2023-03-20 17:11:07 +0900

    Assignment1-1 : 자기소개 컴포넌트 구현

---
+++ src/pages/Introduce/Introduce.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './Introduce.scss';
+
+const Introduce = () => {
+    return(
+        <form className="profileFrame">
+            <div>
+                <img className="profileImg"/>
+                <p className="profileName">신효민</p>
+                <p className="profileSubName">44기</p>
+            </div>
+        </form>
+    )
+}
+
+export default Introduce;
