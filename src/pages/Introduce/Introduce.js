d4b4803f2867a867db216cbd5e0dd145ca7235fa
suny0ung <121158293+suny0ung@users.noreply.github.com>
2023-03-20 20:54:39 +0900

    Assignment1-2 : event 사용하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,14 +1,37 @@
 import React, { useState } from "react";
 import "./Introduce.scss"
 
+function HandleInput (){
+  console.log("input 태그 동작")
+}
+
+function HandleButton(){
+  console.log("button 태그 동작")
+}
+
 function Introduce () {
   return(
     <div className="profileWrap">
       <div className="profileCard">
         <div className="profileImage" alt="profile-img"></div>
         <div className="userName">
-        <span className="name">최선영</span>
-        <span className="thNum">44기</span>
+          <span className="name">최선영</span>
+          <span className="thNum">44기</span>
+        </div>
+        <div className="commentWrap">
+          <input
+            className="input"
+            type="text"
+            placeholder="댓글"
+            onChange={HandleInput}
+          >
+          </input>
+          <button 
+            className="button"
+            onClick={HandleButton}
+          >
+            클릭
+          </button>
         </div>
       </div>
     </div>
