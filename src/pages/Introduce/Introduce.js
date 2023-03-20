50cef7d120cb32ca8140a118cf575ffd6f3fc641
KIMYOUNGWOON <youngwn2130@gmail.com>
2023-03-20 16:25:16 +0900

    Assignment1-1: 자기소개 컴포넌트 구현

---
+++ src/pages/Introduce/Introduce.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Introduce.scss';
+
+const clamp = '>wecode';
+
+function Introduce() {
+  return (
+    <div className="introduce">
+      <div className="blackBox">
+        <div className="rogo">{clamp}</div>
+      </div>
+      <h1>김영운</h1>
+      <h2>44기</h2>
+    </div>
+  );
+}
+
+export default Introduce;
