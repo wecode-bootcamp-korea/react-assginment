9e312f9faddfb00e409443b701442d2fd1790f95
KIMYOUNGWOON <youngwn2130@gmail.com>
2023-03-20 16:51:03 +0900

    Assignment1-2 : event 사용하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -3,6 +3,15 @@ import './Introduce.scss';
 
 const clamp = '>wecode';
 
+function handleInput() {
+  console.log('input 태그 동작');
+}
+
+function handleButton(e) {
+  console.log('button 태그 동작');
+  e.preventDefault();
+}
+
 function Introduce() {
   return (
     <div className="introduce">
@@ -11,6 +20,10 @@ function Introduce() {
       </div>
       <h1>김영운</h1>
       <h2>44기</h2>
+      <form>
+        <input onChange={handleInput} />
+        <button onClick={handleButton}>클릭</button>
+      </form>
     </div>
   );
 }
