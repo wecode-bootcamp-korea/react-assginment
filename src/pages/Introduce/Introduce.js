fb1470aebe32b8a265721f410a1650e408414c80
George Kim <ggkim0614@gmail.com>
2023-03-20 16:39:55 +0900

    Assignment1-2 : event 사용하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -2,6 +2,19 @@ import React from 'react';
 import './Introduce.scss';
 
 const Introduce = () => {
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
+  const handleInput = () => {
+    console.log('input 태그 동작');
+  }
+
+  const handleClick = () => {
+    console.log('button 태그 동작');
+  }
+
   return(
     <div className="container">
       <div className="card">
@@ -9,6 +22,10 @@ const Introduce = () => {
           <div className='card-image'></div>       
           <div className="name">김정환</div>
           <div className="gen-no">44기</div>
+          <form onSubmit={handleSubmit}>   
+            <input type="text" name="input" id="" onInput={handleInput} />
+            <button onClick={handleClick}>클릭</button>  
+          </form>
         </span>
       </div> 
       
