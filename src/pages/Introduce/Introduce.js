c45bc01a082cee795224be8adba0fa618b7988ce
George Kim <ggkim0614@gmail.com>
2023-03-20 16:12:54 +0900

    Assignment1-1 : 자기소개 컴포넌트 구현

---
+++ src/pages/Introduce/Introduce.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Introduce.scss';
+
+const Introduce = () => {
+  return(
+    <div className="container">
+      <div className="card">
+        <span>
+          <div className='card-image'></div>       
+          <div className="name">김정환</div>
+          <div className="gen-no">44기</div>
+        </span>
+      </div> 
+      
+    </div>
+  )
+}
+
+export default Introduce;
