b3c7406d3faf31cb25777c776a323afabdc6f485
신효민 <beanphone@sinhyomin-ui-MacBookPro.local>
2023-03-20 20:03:52 +0900

    Assignment1-2 : event 사용하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,16 +1,40 @@
-import React from "react";
-import './Introduce.scss';
+import React, { useState } from "react";
+import "./Introduce.scss";
 
 const Introduce = () => {
-    return(
-        <form className="profileFrame">
-            <div>
-                <img className="profileImg"/>
-                <p className="profileName">신효민</p>
-                <p className="profileSubName">44기</p>
-            </div>
-        </form>
-    )
-}
+  const [input, setInput] = useState("");
+  const [button, setButton] = useState("");
+
+  const HandleInput = (e) => {
+    console.log("input 태그 동작");
+  };
+
+  const Handlebutton = (e) => {
+    console.log("button 태그 동작");
+  };
+
+  return (
+    <form>
+      <div className="profileFrame">
+        <img className="profileImg" />
+        <span className="profileName">신효민</span>
+        <span className="profileSub">44기</span>
+        <span className="inputArea">
+          <input
+            onChange={(e) => {
+              HandleInput(e);
+            }}
+          />
+          <button
+            onClick={(e) => {
+              Handlebutton(e);
+              e.preventDefault(e);
+            }}
+          > 클릭 </button>
+        </span>
+      </div>
+    </form>
+  );
+};
 
 export default Introduce;
