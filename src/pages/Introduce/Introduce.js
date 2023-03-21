5aae0b8c35a20d031aa867c5b53df257ce0f078a
박지연 <parkjiyeoun@bagjiyeon-ui-MacBookPro.local>
2023-03-21 16:18:55 +0900

    Assignment2-1 : Router 컴포넌트 구현

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,33 +1,36 @@
 import React from "react";
 import "./Introduce.css";
 
-
-
+import { useNavigate } from "react-router-dom";
 
 const IntroduceName = () => {
-    const logoname = "> wecode"
-    const handleInput = () => {
-        console.log("input 태그 동작")
-    }
-    const handleButton = (e) => {
-        e.preventDefault();
-        console.log("button 태그 동작") 
-    }    
-    return (
-        <div className="body">
-            <div className="container">
-              <div className="logobox">
-                <span>{logoname}</span>
-              </div>
-              <span className="name">박지연</span>
-              <span className="class">44기</span>
-              <form>
-                <input type="text" onChange={handleInput} />
-                <button onClick={handleButton}>클릭</button>
-              </form>
-            </div>
+  const navigate = useNavigate();
+
+  const logoname = "> wecode";
+  const handleInput = () => {
+    console.log("input 태그 동작");
+  };
+  const handleButton = (e) => {
+    e.preventDefault();
+    navigate("/product");
+    console.log("button 태그 동작");
+  };
+
+  return (
+    <div className="body">
+      <div className="container">
+        <div className="logobox">
+          <span>{logoname}</span>
         </div>
-    );
-}
+        <span className="name">박지연</span>
+        <span className="class">44기</span>
+        <form>
+          <input type="text" onChange={handleInput} />
+          <button onClick={handleButton}>클릭</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default IntroduceName;
