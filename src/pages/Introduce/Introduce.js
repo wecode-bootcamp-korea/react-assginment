45e6b1996bdc1197e9794167e36f661de991c6ab
Park <hyoster@Hyosungui-MacBookPro.local>
2023-03-23 16:13:01 +0900

    Assignment1-1 : 자기소개 컴포넌트 구현

---
+++ src/pages/Introduce/Introduce.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Introduce.scss";
+
+const Introduce = () => {
+  return (
+    <div className="profile">
+      <div className="center">
+        <div className="inner">
+          <div className="wecode">>wecode</div>
+        </div>
+        <div className="name">박효성</div>
+        <div className="class">44기</div>
+      </div>
+    </div>
+  );
+};
+
+export default Introduce;
