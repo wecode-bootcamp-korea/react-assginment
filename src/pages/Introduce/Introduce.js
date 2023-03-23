8995608b539092fa5ff43593fe0dc40429b34ff6
Park <hyoster@Hyosungui-MacBookPro.local>
2023-03-23 16:37:01 +0900

    Assignment1-2 : event 사용하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -2,6 +2,14 @@ import React from "react";
 import "./Introduce.scss";
 
 const Introduce = () => {
+  function handleButton() {
+    console.log("button 태그 동작");
+  }
+
+  function handleInput() {
+    console.log("input 태그 동작");
+  }
+
   return (
     <div className="profile">
       <div className="center">
@@ -10,6 +18,8 @@ const Introduce = () => {
         </div>
         <div className="name">박효성</div>
         <div className="class">44기</div>
+        <input onChange={handleInput}></input>
+        <button onClick={handleButton}>클릭</button>
       </div>
     </div>
   );
