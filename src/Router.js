e655bc644f0109d552b0d09cfa7edbc5d6d60453
George Kim <ggkim0614@gmail.com>
2023-03-27 01:28:00 +0900

    Assignment4-1 : menu List 상수데이터로 구현

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Introduce from './pages/Introduce/Introduce';
+import Nav from './pages/components/Nav/Nav';
 import Product from './pages/Product/Product';
 import ProductList from './pages/ProductList/ProductList';
 import Review from './pages/Product/components/Review/Review';
@@ -11,14 +12,15 @@ import Color from './pages/Product/components/Color/Color';
 const Router = () => {
   return(
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Introduce />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/productList" element={<ProductList />} />
-        <Route path="/review" element={<Review />} />
-        <Route path="/count" element={<Count />} />
-        <Route path="/color" element={<Color />} />
-      </Routes>
+      <Nav />
+        <Routes>
+          <Route path="/" element={<Introduce />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/productList" element={<ProductList />} />
+          <Route path="/review" element={<Review />} />
+          <Route path="/count" element={<Count />} />
+          <Route path="/color" element={<Color />} />
+        </Routes>
     </BrowserRouter>
   )
 }
