550ff48d854fae543cd3a8dcd119f37983bcfdad
KIMYOUNGWOON <youngwn2130@gmail.com>
2023-03-27 15:57:15 +0900

    Assignment4-2 : ColorCard 상수데이터로 구현하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -2,6 +2,16 @@ import React from 'react';
 import './Introduce.scss';
 import { Link, useNavigate } from 'react-router-dom';
 
+function ColorBox({ content }) {
+  return (
+    <div className="colorBox">
+      <div className={content.classname} />
+      <p>이름 : {content.colorName}</p>
+      <p>rgb : {content.rgb}</p>
+    </div>
+  );
+}
+
 function Introduce() {
   const clamp = '>wecode';
 
@@ -18,21 +28,35 @@ function Introduce() {
   }
 
   return (
-    <div className="introduce">
-      <div className="blackBox">
-        <div className="rogo">{clamp}</div>
+    <>
+      <div className="introduce">
+        <div className="blackBox">
+          <div className="rogo">{clamp}</div>
+        </div>
+        <h1>김영운</h1>
+        <h2>44기</h2>
+        <form>
+          <input onChange={handleInput} />
+          <button onClick={handleButton}>클릭</button>
+        </form>
+        <Link className="link" to="/product">
+          product 페이지로 이동
+        </Link>
       </div>
-      <h1>김영운</h1>
-      <h2>44기</h2>
-      <form>
-        <input onChange={handleInput} />
-        <button onClick={handleButton}>클릭</button>
-      </form>
-      <Link className="link" to="/product">
-        product 페이지로 이동
-      </Link>
-    </div>
+      <div className="colorBoxWrap">
+        {COLOR_BOX_CONTENT.map((content) => {
+          return <ColorBox content={content} />;
+        })}
+      </div>
+    </>
   );
 }
 
 export default Introduce;
+
+const COLOR_BOX_CONTENT = [
+  { colorName: 'red', rgb: '255, 0, 0', classname: 'redBox' },
+  { colorName: 'yellow', rgb: '255, 255, 0', classname: 'yellowBox' },
+  { colorName: 'green', rgb: '0, 128, 0', classname: 'greenBox' },
+  { colorName: 'blue', rgb: '0, 0, 255', classname: 'blueBox' },
+];
