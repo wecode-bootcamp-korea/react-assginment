cb7bc33be3dd0926d51e2eebd532107ab5044385
박지연 <parkjiyeoun@bagjiyeon-ui-MacBookPro.local>
2023-03-28 19:52:07 +0900

    Assignment4-2 : ColorCard 상수데이터로 구현하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -19,23 +19,48 @@ const Introduce = () => {
   };
 
   return (
-    <div className="introduce">
-      <div className="container">
-        <div className="logobox">
-          <span>{logoname}</span>
+    <div className="body">
+      <div className="introduce">
+        <div className="container">
+          <div className="logobox">
+            <span>{logoname}</span>
+          </div>
+          <span className="name">박지연</span>
+          <span className="class">44기</span>
+          <form>
+            <input type="text" onChange={handleInput} />
+            <button onClick={handleButton}>클릭</button>
+            <Link to="/product" className="link">
+              상품보러 가기
+            </Link>
+          </form>
         </div>
-        <span className="name">박지연</span>
-        <span className="class">44기</span>
-        <form>
-          <input type="text" onChange={handleInput} />
-          <button onClick={handleButton}>클릭</button>
-          <Link to="/product" className="link">
-            상품보러 가기
-          </Link>
-        </form>
       </div>
+      <ul className="color-box">
+        {COLOR.map((colorlist) => (
+          <li>
+            <div
+              className="color-container"
+              style={{ backgroundColor: colorlist.color }}
+            />
+            <p className="color-name">이름 : {colorlist.color}</p>
+            <p className="rgb-name">rgb : {colorlist.rgb}</p>
+          </li>
+        ))}
+        {/* <div className="color-container">
+      <span className="color-name">이름 : red</span>
+      <span className="rgb-name">rgb : 255,0,0</span>
+    </div> */}
+      </ul>
     </div>
   );
 };
 
 export default Introduce;
+
+const COLOR = [
+  { id: 1, color: "red", rgb: "255, 0, 0" },
+  { id: 2, color: "yellow", rgb: "255, 255, 0" },
+  { id: 3, color: "green", rgb: "0, 128, 0" },
+  { id: 4, color: "blue", rgb: "0, 0, 255" },
+];
