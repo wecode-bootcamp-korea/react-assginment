2a768785c417be07acfd1e95f939c97604945564
suny0ung <121158293+suny0ung@users.noreply.github.com>
2023-03-29 12:35:59 +0900

    Assignment4-4 : productList 페이지 mock Data 구현

--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -1,69 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ProductList.scss";
 
 const ProductList = () => {
+
+  const [ catsData, setCatsData] = useState([]);
+
+  useEffect(()=>{
+    fetch("/data/cat.json")
+      .then(response => response.json())
+      .then(result => setCatsData(result));
+  },[])  
+
   return (
     <div className="productList">
       <h2 className="title">고양이 리스트</h2>
-      <div className="listContainer">
-        <div className="listBox">
-          <img className="listImage" src="/images/cat.jpeg" alt="cat" />
-          <span className="listTitle">고양이</span>
-          <span>나이 : 4살</span>
-          <span>성별 : male</span>
-          <span>거주지 : 춘천</span>
-        </div>
-        <div className="listBox">
-          <img className="listImage" src="/images/cat.jpeg" alt="cat" />
-          <span className="listTitle">고냥이</span>
-          <span>나이 : 14살</span>
-          <span>성별 : female</span>
-          <span>거주지 : 양평</span>
-        </div>
-        <div className="listBox">
-          <img className="listImage" src="/images/cat.jpeg" alt="cat" />
-          <span className="listTitle">고용이</span>
-          <span>나이 : 7살</span>
-          <span>성별 : male</span>
-          <span>거주지 : 천안</span>
-        </div>
-        <div className="listBox">
-          <img className="listImage" src="/images/cat.jpeg" alt="cat" />
-          <span className="listTitle">모울이</span>
-          <span>나이 : 1살</span>
-          <span>성별 : female</span>
-          <span>거주지 : 부산</span>
-        </div>
-        <div className="listBox">
-          <img className="listImage" src="/images/cat.jpeg" alt="cat" />
-          <span className="listTitle">곰율이</span>
-          <span>나이 : 3살</span>
-          <span>성별 : male</span>
-          <span>거주지 : 노원</span>
-        </div>
-        <div className="listBox">
-          <img className="listImage" src="/images/cat.jpeg" alt="cat" />
-          <span className="listTitle">고울이</span>
-          <span>나이 : 11살</span>
-          <span>성별 : male</span>
-          <span>거주지 : 평양</span>
-        </div>
-        <div className="listBox">
-          <img className="listImage" src="/images/cat.jpeg" alt="cat" />
-          <span className="listTitle">고물이</span>
-          <span>나이 : 9살</span>
-          <span>성별 : male</span>
-          <span>거주지 : 전주</span>
-        </div>
-        <div className="listBox">
-          <img className="listImage" src="/images/cat.jpeg" alt="cat" />
-          <span className="listTitle">기양이</span>
-          <span>나이 : 2살</span>
-          <span>성별 : male</span>
-          <span>거주지 : 충청</span>
-        </div>
+      {/* <div className="listContainer"> */}
+      <div className="listContainer" >
+        {catsData.map(cat=>(
+              <div className="listBox" key={cat.id} >
+                <img className="listImage" src="/images/cat.jpeg" alt="cat" />
+                <span className="listTitle"> {cat.name} </span>
+                <span>나이 : {cat.age} 살</span>
+                <span>성별 : {cat.gender}</span>
+                <span>거주지 : {cat.address}</span>
+              </div>
+          ))}
       </div>
     </div>
+    // </div>
   );
 };
 
