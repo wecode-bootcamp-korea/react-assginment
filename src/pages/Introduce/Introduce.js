af16a5e99a627eecb488d90b3ead0359b9753d19
hyunah <qkrgusdk0303@naver.com>
2023-04-18 22:40:43 +0900

    Assignment1-2 : event 사용하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -3,12 +3,25 @@ import './Introduce.scss';
 
 
 const Introduce = () => {
+    const handleInput = (e) => {
+        console.log('input 태그 동작')
+        // console.log(e.target.value)
+    }
+
+    const handleButton = () => {
+        console.log('button 태그 동작')
+    }
+
+
+
     return(
         <div className = 'Introduce'>
             <div className = 'profileBox'>
                 <img alt = 'mm' className= 'profile' src = "/images/mm.jpg" width = '300'></img>
                 <h1>박현아</h1>
                 <p>45기</p>
+                <input className = 'txt' type ='text' onChange={handleInput} ></input>
+                <input className = 'btn' type ='button' value="클릭" onClick={handleButton}></input>
             </div>
         </div>
     );
