e1598392e56e27c882659a19652419808426ec15
min2oyo <min2u@outlook.com>
2023-04-18 20:41:10 +0900

    Assignment1-1 : 자기소개 컴포넌트 구현

---
+++ src/pages/Introduce/Introduce.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Introduce.scss";
+
+const Introduce = () => {
+  return (
+    <>
+      <div className="card">
+        <div className="proImg"></div>
+        <div className="name">
+          <span>양진민</span>
+        </div>
+        <div className="numTh">
+          <span>45기</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Introduce;
