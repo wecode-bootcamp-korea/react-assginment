5d3bcd80ba33320156cc1704999fc0407eb957d0
김준섭 <junsobi@gmail.com>
2023-04-18 22:08:23 +0900

    assignment 1-2 완료

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,6 +1,14 @@
 import "./Introduce.scss";
 
 const Introduce = () => {
+  const handleInput = () => {
+    console.log("input 태그 동작");
+  };
+
+  const handleButton = () => {
+    console.log("button 태그 동작");
+  };
+
   return (
     <div className="introduce">
       <div className="wrapper">
@@ -9,6 +17,13 @@ const Introduce = () => {
         <div className="myDetail">
           <div className="myName">김준섭</div>
           <div className="numb">45기</div>
+        </div>
+
+        <div className="inBtn">
+          <input className="inputs" onChange={handleInput}></input>
+          <button className="btns" onClick={handleButton}>
+            버튼
+          </button>
         </div>
       </div>
     </div>
