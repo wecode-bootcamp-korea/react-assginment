5cb16bd0e88522d7384720f3dc22827e30250006
taewon kim <taewon2659@gmail.com>
2023-04-19 01:03:03 +0900

    Assignment2-2 : Sass 적용

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -6,9 +6,9 @@ const handleInput = () => {
   console.log('input 태그 동작');
 };
 
-const hnadleButton = () => {
-  console.log('button 태그 동작');
-};
+// const hnadleButton = () => {
+//   console.log('button 태그 동작');
+// };
 
 
 const Introduce = () => {
@@ -21,11 +21,15 @@ const Introduce = () => {
   return (
     <div className="introduce">
       <article className="container">
-        <img alt="main_img" className="profileImg" src="images/golf-ball-yellow.jpg"></img>
-        <p className="profileName">김태원</p>
-        <p className="generation">45기</p>
-        <input type="text" className="inputBox" onChange={handleInput}></input>
-        <button className="post" onClick={goToProduct}>클릭</button>
+        <img alt="main_img" className="profileImg" src="images/profile.jpg"></img>
+        <div className="userInfoBox">
+          <p className="profileName">김태원</p>
+          <p className="generation">45기</p>
+        </div>
+        <div className="eventBox">
+          <input type="text" className="inputBox" onChange={handleInput}></input>
+          <button className="post" onClick={goToProduct}>클릭</button>
+        </div>
         <p><Link to='/product'>product 페이지로 이동</Link></p>
 
       </article>
