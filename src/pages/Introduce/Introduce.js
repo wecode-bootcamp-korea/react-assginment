16a4471d1bd77a3a88934353df7d6b1d5634c8bd
김준섭 <junsobi@gmail.com>
2023-04-18 21:56:11 +0900

    assignment 1번

---
+++ src/pages/Introduce/Introduce.js
@@ -0,0 +1,18 @@
+import "./Introduce.scss";
+
+const Introduce = () => {
+  return (
+    <div className="introduce">
+      <div className="wrapper">
+        <img src="/images/cat.jpeg"></img>
+
+        <div className="myDetail">
+          <div className="myName">김준섭</div>
+          <div className="numb">45기</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Introduce;
