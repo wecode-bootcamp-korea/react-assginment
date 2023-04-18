9f21d3ff1e12c759db225c7cfae6bd9d61e6b176
Patata <patattta93@gmail.com>
2023-04-18 17:12:37 +0900

    Assignment2-1 : Router 컴포넌트 구현

---
+++ src/pages/Introduce/Introduce.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "./Introduce.css";
+import { Link } from "react-router-dom";
+
+const Introduce = () => {
+  return (
+    <div className="container">
+      <div className="wrapper">
+        <img className="profile-img" src="/images/cat.jpeg" alt="" />
+        <div className="profile-info">
+          <div className="name">김희연</div>
+          <div className="cohort-number">45기</div>
+          <Input />
+          <Button />
+          <div>
+            <Link to="/product">product 페이지로 이동</Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+const handleInput = () => {
+  console.log("input 태그 동작");
+};
+
+const handleButton = () => {
+  console.log("button 태그 동작");
+};
+
+export const Input = () => {
+  return <input type="text" className="input" onChange={handleInput} />;
+};
+
+export const Button = () => {
+  return (
+    <button className="btn" onClick={handleButton}>
+      클릭
+    </button>
+  );
+};
+
+export default Introduce;
