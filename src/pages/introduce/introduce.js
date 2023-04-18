5fc046cf9d1801bc90ab8343311537090928b5e0
Jinny-Jin <katie9796@naver.com>
2023-04-18 17:46:19 +0900

    Assignment1-2 : event 사용하기

--- src/pages/introduce/introduce.js
+++ src/pages/introduce/introduce.js
@@ -1,19 +1,28 @@
 import React from "react";
 import "./introduce.scss";
 
+function handleInput() {
+  console.log("input 태그 동작");
+}
+
+function handleButton() {
+  alert("잘 부탁드립니다");
+  console.log("button 태그 동작");
+}
+
 const Profile = () => {
   return (
-    <div class="container">
+    <div className="container">
       <img src="/images/me.jpg" />
-      <p class="content" id="이경진">
+      <p className="content" id="이경진">
         이경진
       </p>
-      <p class="content" id="기수">
+      <p className="content" id="기수">
         45기
       </p>
-      <div class="search">
-        <input type="text"></input>
-        <button>클릭</button>
+      <div className="search">
+        <input type="text" onChange={handleInput}></input>
+        <button onClick={handleButton}>클릭</button>
       </div>
     </div>
   );
