d31ac67f0e995f892918679a7e29910c61b73499
taewon kim <taewon2659@gmail.com>
2023-04-18 20:58:37 +0900

    Assignment2-1 : Router 컴포넌트 구현

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,15 +1,23 @@
 import React from "react";
-import './Introduce.scss'
+import './Introduce.scss';
+import { Link, useNavigate } from 'react-router-dom';
 
 const handleInput = () => {
   console.log('input 태그 동작');
-}
+};
 
 const hnadleButton = () => {
   console.log('button 태그 동작');
-}
+};
+
 
 const Introduce = () => {
+  
+  const navigate = useNavigate();
+  const goToProduct = () => {
+    navigate('/product');
+  };
+
   return (
     <div className="introduce">
       <article className="container">
@@ -17,7 +25,9 @@ const Introduce = () => {
         <p className="profileName">김태원</p>
         <p className="generation">45기</p>
         <input type="text" className="inputBox" onChange={handleInput}></input>
-        <button className="post" onClick={hnadleButton}>클릭</button>
+        <button className="post" onClick={goToProduct}>클릭</button>
+        <p><Link to='/product'>product 페이지로 이동</Link></p>
+
       </article>
     </div>
   );
