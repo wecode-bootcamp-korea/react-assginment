1ed43fa2df6ab3581cc5ccf85ee59b1151a93e2c
dlfrlwkd11@daum.net <dlfrlwkd11@daum.net>
2023-04-18 19:05:10 +0900

    Assignment1-2 : event 사용하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,11 +1,22 @@
 import React from "react"
 import './Introduce.css'
+
 const Introduce = () => {
+  const handleInput = () => {
+    console.log("input 태그 동작")
+  }
+  
+  const handleButton = () => {
+    console.log("button 태그 동작")
+  }
+
   return (
     <div className="introduce">
       <img src="images/jellyfish.jpg" alt="해파리 사진" />
       <h2>김수정</h2>
       <h3>45기</h3>
+      <input type="text" onChange={handleInput}></input>
+      <button onClick={handleButton}>버튼</button>
     </div>
   )
 } 
