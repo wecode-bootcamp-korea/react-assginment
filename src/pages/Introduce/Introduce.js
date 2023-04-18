4b569a9bbe677eb29fdf177a8a851d299ab62ae3
jeonseungbeom <7237sbj@gmail.com>
2023-04-18 20:30:50 +0900

    Assignment1-1 : 자기소개 컴포넌트 구현

---
+++ src/pages/Introduce/Introduce.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../Introduce/Introduce.scss'
+
+const Introduce = () => {
+  return (
+    <div className='box'>
+        <img className='picture' src='/images/cat.jpeg'></img>
+        <h1>전승범</h1>
+        <h2>45기</h2>
+    </div>
+  )
+}
+
+export default Introduce
