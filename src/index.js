80b18d75487cb30bd6dafd66c8c9e68a8a5c542a
min2oyo <min2u@outlook.com>
2023-04-18 22:58:46 +0900

    Assignment2-2 : Sass 적용

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import ReactDOM from "react-dom/client";
 import "./style/reset.scss";
 import "./style/common.scss";
+import "./style/variables.scss";
+import "./style/mixin.scss";
 import Router from "./Router";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
