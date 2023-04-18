bee47b8c71d627093e0f55d9fe10f72af3e73782
hyunah <qkrgusdk0303@naver.com>
2023-04-19 01:06:11 +0900

    Assignment2-2 : Sass 적용

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -22,8 +22,9 @@ const Introduce = () => {
         <div className = 'Introduce'>
             <div className = 'profileBox'>
                 <img alt = 'mm' className= 'profile' src = "/images/mm.jpg" width = '300'></img>
-                <h1>박현아</h1>
-                <p>45기</p>
+                <div></div>
+                <div className= 'name'>박현아</div>
+                <div className= 'generation'>45기</div>
                 <input className = 'txt' type ='text' onChange={handleInput} ></input>
                 <input className = 'btn' type ='button' value="클릭" onClick={handleButton}></input>
                 
