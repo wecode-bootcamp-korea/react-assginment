20423ac23fc5018654fa5196bc9645f988f067a2
yoohmoon <ansekfzhd5455@naver.com>
2023-04-18 21:22:57 +0900

    Assignment1-2 : event 사용하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -2,13 +2,29 @@ import React from "react";
 import "../Introduce/Introduce.css";
 
 function Introduce() {
+  const handleButton = () => {
+    console.log("button 태그 동작");
+  };
+
+  const handleInput = () => {
+    console.log("input 태그 동작");
+  };
+
   return (
-    <div className="introduce">
+    <div className="introduce" style={{ boxSizing: "border-box" }}>
       <div className="userProfileContainer">
         <img alt="user profile image" src="/images/cat.jpeg" />
-        <div>
-          <p>문유현</p>
-          <p>45기</p>
+        <div className="bottomWrapper">
+          <div>
+            <p>문유현</p>
+            <p>45기</p>
+          </div>
+          <form>
+            <input type="text" onChange={handleInput} />
+            <button type="button" onClick={handleButton}>
+              클릭
+            </button>
+          </form>
         </div>
       </div>
     </div>
