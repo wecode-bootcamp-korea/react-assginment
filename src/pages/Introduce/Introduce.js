803060e4ed9a21ac0fa072eebc171abb710f51b0
taewon kim <taewon2659@gmail.com>
2023-04-18 17:47:17 +0900

    Assignment1-2 : event 사용하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,6 +1,13 @@
 import React from "react";
 import './Introduce.scss'
 
+const handleInput = () => {
+  console.log('input 태그 동작');
+}
+
+const hnadleButton = () => {
+  console.log('button 태그 동작');
+}
 
 const Introduce = () => {
   return (
@@ -9,6 +16,8 @@ const Introduce = () => {
         <img alt="main_img" className="profileImg" src="images/golf-ball-yellow.jpg"></img>
         <p className="profileName">김태원</p>
         <p className="generation">45기</p>
+        <input type="text" className="inputBox" onChange={handleInput}></input>
+        <button className="post" onClick={hnadleButton}>클릭</button>
       </article>
     </div>
   );
