e9b781a92220d55c061b39c503f29504ef5b08fd
dlfrlwkd11@daum.net <dlfrlwkd11@daum.net>
2023-04-18 17:40:26 +0900

    Assignment1-1 : 자기소개 컴포넌트 구현

---
+++ src/pages/Introduce/Introduce.js
@@ -0,0 +1,13 @@
+import React from "react"
+import './Introduce.css'
+const Introduce = () => {
+  return (
+    <div className="introduce">
+      <img src="images/jellyfish.jpg" alt="해파리 사진" />
+      <h2>김수정</h2>
+      <h3>45기</h3>
+    </div>
+  )
+} 
+
+export default Introduce
