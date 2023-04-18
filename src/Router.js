6587cf4f57559187e321727fe9101c24ffc23cf6
mac <mac@macui-MacBookAir.local>
2023-04-18 22:00:34 +0900

    Assignment2-1 : Router 컴포넌트 구현

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Introduce from './pages/Introduce/Introduce'
+import Product from './pages/Product/Product';
 
   const Router = () => {
     return (
@@ -8,6 +9,7 @@ import Introduce from './pages/Introduce/Introduce'
                                        
         <Routes>
           <Route path="/" element={<Introduce />} />
+          <Route path="/product" element={<Product />} />
         </Routes>
                                       
       </BrowserRouter>
--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './Introduce.scss';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Introduce = () => {
     const handleInput = () => {
       console.log("input 태그 동작");
     }
 
-      function handleButton() {
-        console.log("button 태그 동작");
-      }
+    const navigate = useNavigate();
+    const goToProduct = () => {
+      navigate('/product');
+    }
   
   return (
       <>
@@ -20,9 +23,13 @@ const Introduce = () => {
                   <div className = "name1">이수빈</div>
                   <div>45기</div>
                   <div className='inputbox'>
-                    <input className ="input" type='text' onChange={handleInput}></input>
-                    <button onClick={handleButton}>로그인</button>
+                    <input className ="input"
+                    type='text'onChange={handleInput}>
+                    </input>
+                    <button onClick={goToProduct}>로그인
+                    </button>
                   </div>
+                  <Link to="/product">product 페이지로 이동</Link>
               </div>
             </div>
         </div>
