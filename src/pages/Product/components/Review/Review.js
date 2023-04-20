d6be88f01a7e44fa54c31311b2a316ea44ab1c79
jeonseungbeom <7237sbj@gmail.com>
2023-04-20 17:14:49 +0900

    Assignment3-3 : Review 컴포넌트 구현

--- src/pages/Product/components/Review/Review.js
+++ src/pages/Product/components/Review/Review.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Review.scss";
 
 const Review = () => {
+  const [summary, setSummary] = useState(Boolean);
+
   return (
     <div className="review">
       <div className="user">
@@ -13,13 +15,13 @@ const Review = () => {
       </div>
       <div className="reviewTitle">배송도 빠르고 정말 좋아요.</div>
       <div className="reviewMain">
-        <div className="reviewMainContents">
+        <div className={summary?"hide":"reviewMainContents"}>
           주문한지 하루만에 배송이 도착해서 너무 좋았습니다.
           <br />
           다음에도 필요하면 또 여기서 시킬 것 같아요.
         </div>
-        <span className="reviewMainHandler" onClick={() => {}}>
-          더보기 ▼
+        <span className="reviewMainHandler" onClick={()=>{setSummary(!summary)}}>
+          {summary? '더보기 ▼' : '접기 ▲'}
         </span>
       </div>
     </div>
