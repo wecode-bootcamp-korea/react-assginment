39794f6b4e4e2c488f186dbaa4dd5a97ac537e73
Jinny-Jin <katie9796@naver.com>
2023-04-20 13:30:50 +0900

    수정사항 1 Router.js에 모든 경로 지정

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Profile from "./pages/introduce/introduce";
 import Product from "./pages/Product/Product";
 import ProductList from "./pages/ProductList/ProductList";
+import Review from "./pages/Product/components/Review/Review";
+import Count from "./pages/Product/components/Count/Count";
+import Color from "./pages/Product/components/Color/Color";
 
 const Router = () => {
   return (
@@ -11,6 +14,9 @@ const Router = () => {
         <Route path="/" element={<Profile />} />
         <Route path="/product" element={<Product />} />
         <Route path="/productList" element={<ProductList />} />
+        <Route path="/review" element={<Review />} />
+        <Route path="/count" element={<Count />} />
+        <Route path="/color" element={<Color />} />
       </Routes>
     </BrowserRouter>
   );
