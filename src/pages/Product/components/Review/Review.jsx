78376d0241e0fa41e2b99efe624e91322828baff
min2oyo <min2u@outlook.com>
2023-04-20 15:27:12 +0900

    Refactor: Reveiw 인라인 스타일 제거 | 함수 간소화

--- src/pages/Product/components/Review/Review.jsx
+++ src/pages/Product/components/Review/Review.jsx
@@ -6,9 +6,6 @@ const Review = () => {
   // 변수
   const [isExtend, setIsExtend] = useState(false);
 
-  // 함수
-  const extend = () => isExtend ? setIsExtend(false) : setIsExtend(true);
-
   // 출력
   return (
     <>
@@ -22,12 +19,12 @@ const Review = () => {
         </div>
         <div className="reviewTitle">배송도 빠르고 정말 좋아요.</div>
         <div className="reviewMain">
-          <div className="reviewMainContents" style={{ display: isExtend ? `block` : `none` }}>
+          <div className={`reviewMainContents ${isExtend ? 'isExtendTrue' : 'isExtendFalse'}`} >
             주문한지 하루만에 배송이 도착해서 너무 좋았습니다.
             <br />
             다음에도 필요하면 또 여기서 시킬 것 같아요.
           </div>
-          <span className="reviewMainHandler" onClick={() => extend()}>
+          <span className="reviewMainHandler" onClick={() => isExtend ? setIsExtend(false) : setIsExtend(true)}>
             {isExtend ? `접기 ▲` : `더보기 ▼`}
           </span>
         </div>
