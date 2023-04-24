07a81f7ec4bc50bc9de023bb7f43ee89a3c482d0
오승민 <seungmin@oseungmin-ui-noteubug.local>
2023-04-24 22:14:52 +0900

    Assignment2-1 : Router 컴포넌트 구현

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "./Introduce.scss";
+import { Link, useNavigate } from "react-router-dom";
 
 const Introduce = () => {
+
+    const navigate = useNavigate();
 
     function handleInput() {
         console.log("input 태그 동작");
@@ -9,22 +12,34 @@ const Introduce = () => {
 
     function handleButton() {
         console.log("button 태그 동작");
+        navigate('/product');
       }
+
+
+      
+
+    //   const goToProduct = () => {
+    //     navigate('/product');
+    //   }
 
      
 
     return(
         <div className="container">
             <div className="wecode">
-                <div className="text">>wecode</div>
+                <div className="text">wecode</div>
 
             </div>
             <p className="name">오승민</p>
             <p>45기</p>
             <input type="text" onChange={handleInput}></input>
             <button onClick={handleButton}>클릭</button>
+            <p>
+               <Link to = "/product">product페이지로 이동</Link> 
+            </p>
+          
         </div>
-    
+         
     )
 }
 
