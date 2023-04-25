dc6b395cdabe3263d3dfab58364853d168b28b1e
이원준 <wonjun@MacBook-Pro-2.local>
2023-04-25 20:43:05 +0900

    2-1완성
    git push origin assignment/45-wonjun
    git commit-m 2-1완성
    git commit - m 2-1완성

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Introduce from "./pages/Introduce/Introduce"; 
+import Router from './Router' //라우터연결 방법
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Introduce/>);
+root.render(<Router/>);
+
+
+//render 안 변수를 바꾸기 위해 라우터를 사용 
--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Introduce.scss";
+import { Link } from "react-router-dom";
+import {useNavigate} from "react-router-dom"
 
 //모양 기억하기 한js 파일내에 컴포넌트 여러개 넣을때 쓰는 법//
 // export const App= ()=> {
@@ -25,6 +27,15 @@ const Introduce=() => {
     console.log("button 태그 동작");
   }
 
+  const navigate = useNavigate(
+  );
+  const goToProduct = () => {                            // 3
+    navigate('/product');
+  };
+
+
+
+
   return (
     <div className="introduce">
         <div className="weCodeBox">       
@@ -36,6 +47,9 @@ const Introduce=() => {
         <input type="text" onChange={handleInput} />
       </div>
       <button className="button" onClick={handleButton}>버튼</button>
+      <button className="loginBtn" onClick={goToProduct}>클릭</button>
+      
+      <Link to='/product'> Product 페이지로 이동 </Link>
       </div>
   );
 };
