cd9ce244ae1bceacd7ccb63a9e63b465a6ad59c6
joyfuldev <wings2132@naver.com>
2023-04-25 11:47:51 +0900

    Assignment4-3 : product 페이지 mock Data 구현

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ColorButton from "./components/ColorButton/ColorButton";
 import Color from "./components/Color/Color";
 import Count from "./components/Count/Count";
@@ -8,24 +8,35 @@ import "./Product.scss";
 const Product = () => {
   const [color, setColor] = useState("white");
   const [count, setCount] = useState(1);
+  const [product, setProduct] = useState();
 
   const price = 300;
   const totalPrice = price * count;
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const response = await fetch("data/data.json", { method: "GET" });
+    const data = await response.json();
+    setProduct(data);
+  };
 
   return (
     <div className="product">
       <div className="productDetail">
         <div className="productDetailImg">
           <img
-            src={`/images/golf-ball-${color}.jpg`} // color 이름에 따라 다른 이미지 경로 넣기
+            src={product?.img + `${color}.jpg`} // color 이름에 따라 다른 이미지 경로 넣기
             alt={`golf-ball`}
           />
           <ColorButton color={color} setColor={setColor} />
         </div>
         <div className="productDetailInfo">
-          <span className="title">골프공</span>
+          <span className="title">{product?.title}</span>
           <span>비거리를 비약적으로 늘려줍니다</span>
-          <span>가격 : {price.toLocaleString()} 원</span>
+          <span>가격 : {product?.price.toLocaleString()} 원</span>
           <Color color={color} setColor={setColor} />
           <div className="quantity">
             <span> 수량 : </span>
@@ -39,7 +50,7 @@ const Product = () => {
         <div className="reviewListHeader">
           <span>상품평</span>
         </div>
-        <Review />
+        <Review review={product?.review} />
       </div>
     </div>
   );
