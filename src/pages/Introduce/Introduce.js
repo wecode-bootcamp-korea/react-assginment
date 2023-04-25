d7e2bb3b88ae4c767b9466746218fb3d0cd52347
taewon kim <taewon2659@gmail.com>
2023-04-25 21:18:27 +0900

    Assignment4-2 : ColorCard 상수데이터로 구현하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,8 +1,9 @@
-import React from "react";
-import { Link, useNavigate } from 'react-router-dom';
+import React,{useState} from "react";
+import { Link, useNavigate} from 'react-router-dom';
 import './Introduce.scss';
 
 const Introduce = () => {
+  const [box, useBox] = useState();
   
   const handleInput = () => {
     console.log('input 태그 동작');
@@ -27,8 +28,29 @@ const Introduce = () => {
         </div>
         <p><Link to='/product'>product 페이지로 이동</Link></p>
       </article>
+      <div className="cardContainer">
+        {COLOR.map((c) => {
+          return (
+            <div className="cardBox">
+              <div className="mainColor" style={{backgroundColor:`rgb(${c.rgb})`}}></div>
+              {console.log(`rbg(${c.rgb})`)}
+              <div className="textArea">
+                <p>이름 : {c.color}</p>
+                <p> rgb : {c.rgb}</p>
+              </div>
+            </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
 
 export default Introduce;
+
+const COLOR = [
+  {id:1, color:'red', rgb:"255,0,0"},
+  {id:2, color:'yellow', rgb:"255, 255, 0"},
+  {id:3, color:'green', rgb:"0, 128, 0"},
+  {id:4, color:'blue',  rgb:"0, 0, 255"}
+]
