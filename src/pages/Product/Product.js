cb86d425fcb3836367195da360f29a9929205eb4
sojin lee <sojin626@naver.com>
2023-04-26 01:24:56 +0900

    Assignment4-3 : product 페이지 mock Data 구현

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ColorButton from "./components/ColorButton/ColorButton";
 import Color from "./components/Color/Color";
 import Count from "./components/Count/Count";
@@ -8,23 +8,31 @@ import "./Product.scss";
 const Product = () => {
   const [color, setColor] = useState("white");
   const [count, setCount] = useState(1);
+  const [product, setProduct] = useState({});
+
   const price = 300;
   const totalPrice = price * count;
+
+  useEffect(() => {
+    fetch("/data/data.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setProduct(data);
+      });
+  }, []);
+
   return (
     <div className="product">
       <div className="productDetail">
         <div className="productDetailImg">
-          <img
-            src={`/images/golf-ball-${color}.jpg`} // color 이름에 따라 다른 이미지 경로 넣기
-            alt={`golf-ball`}
-          />
+          <img src={product?.img + `${color}.jpg`} alt={`golf-ball`} />
           {/* ColorButton 컴포넌트 위치 */}
           <ColorButton color={color} setColor={setColor} />
         </div>
         <div className="productDetailInfo">
-          <span className="title">골프공</span>
+          <span className="title">{product.title}</span>
           <span>비거리를 비약적으로 늘려줍니다</span>
-          <span>가격 : {price.toLocaleString()} 원</span>
+          <span>가격 : {product.price?.toLocaleString()} 원</span>
           {/* Color 컴포넌트 위치 */}
           <Color color={color} setColor={setColor} />
           <div className="quantity">
@@ -41,9 +49,10 @@ const Product = () => {
           <span>상품평</span>
         </div>
         {/* Review 컴포넌트 위치 */}
-        <Review />
+        <Review review={product.review} />
       </div>
     </div>
   );
 };
+
 export default Product;
