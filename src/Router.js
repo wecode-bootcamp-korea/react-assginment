7514b5d8ca91db45c30d46099df57bd46d5dc8bd
이원준 <wonjun@MacBook-Pro-2.local>
2023-04-25 23:18:40 +0900

    Assignment3-2 : Count 컴포넌트 구현

--- src/Router.js
+++ src/Router.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Introduce from './pages/Introduce/Introduce';     
 import Product from './pages/Product/Product';               
 import Color from './pages/Product/components/Color/Color'; 
+import Count from './pages/Product/components/Count/Count';
 
 const Router = () => {
   return (
@@ -11,6 +12,7 @@ const Router = () => {
         <Route path='/' element={<Introduce />} />
         <Route path='/product'element={<Product />} />
         <Route path='/color'element={<Color />} />
+        <Route path='/count' element={<Count/>} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Product/components/Count/Count.js
+++ src/pages/Product/components/Count/Count.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Count.scss";
 
-const Count = () => {
+
+const Count= () => { 
+  const [count, setCount] = useState(1)
+
+  const decreaseCount = ()=> {
+    if (count>1){
+      setCount(count -1)
+    }
+  }
+  const increaseCount = ()=> {
+      setCount(count +1)
+  }
+  
+  const resetCount = ()=> {
+    setCount(1)  
+  }
+   
   return (
-    <div className="count">
-      <div className="countInput">
-        <button onClick={() => {}}>-</button>
-        <div className="countInputText">1</div>
-        <button onClick={() => {}}>+</button>
-      </div>
-      <button className="resetBtn" onClick={() => {}}>
-        reset
-      </button>
-    </div>
+   
+  <div>
+      <button onClick={decreaseCount}>-</button>
+      <div>{count}</div>
+      <button onClick={increaseCount}>+</button>
+      <button onClick={resetCount}>reset</button>
+  </div>
+
   );
 };
 
