eec0939018be2b73bda2069a1b6d8bf9bd13b102
jeonseungbeom <7237sbj@gmail.com>
2023-04-25 17:45:59 +0900

    Assignment4-1 : menu List 상수데이터로 구현

--- src/pages/components/Nav/Nav.js
+++ src/pages/components/Nav/Nav.js
@@ -22,10 +22,9 @@ const Nav = () => {
       </div>
       {isOpenMenu && (
         <div className="menuBox">
-          <Link to="/">메인으로 가기</Link>
-          <Link to="/review">리뷰 컴포넌트 가기</Link>
-          <Link to="/count">카운트 컴포넌트 가기</Link>
-          <Link to="/color">컬러 컴포넌트 가기</Link>
+          {menuList.map((menu) => (
+            <Link to={menu.goTo}>{menu.title}</Link>
+          ))}
         </div>
       )}
     </div>
@@ -33,3 +32,10 @@ const Nav = () => {
 };
 
 export default Nav;
+
+const menuList = [
+  { id: 1, goTo: "/", title: "메인으로 가기" },
+  { id: 2, goTo: "/review", title: "리뷰 컴포넌트 가기" },
+  { id: 3, goTo: "/count", title: "카운트 컴포넌트 가기" },
+  { id: 4, goTo: "/color", title: "컬러 컴포넌트 가기" },
+];
