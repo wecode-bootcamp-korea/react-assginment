b49a4fabdc875d1eccbd717570f436ef628b8aef
joyfuldev <wings2132@naver.com>
2023-04-26 10:40:19 +0900

    Modify: count state에 초기값 추가

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -7,8 +7,8 @@ import "./Product.scss";
 
 const Product = () => {
   const [color, setColor] = useState("white");
-  const [product, setProduct] = useState();
-  const [count, setCount] = useState();
+  const [product, setProduct] = useState({});
+  const [count, setCount] = useState(1);
 
   const price = 300;
   const totalPrice = price * count;
