71efdb6c38aa9e6ef657c7d42b5a13eb59451392
김준섭 <junsobi@gmail.com>
2023-04-26 12:24:34 +0900

    Assignment4-2 : ColorCard 상수데이터로 구현하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -32,8 +32,29 @@ const Introduce = () => {
           <Link to="/product">Product 페이지로 이동</Link>
         </div>
       </div>
+      <div className="colorCards">
+        {COLOR_CARD.map((colorCard) => (
+          <div className="colorBox">
+            <div
+              className="colorBg"
+              style={{ backgroundColor: `rgb(${colorCard.rgb})` }}
+            ></div>
+            <div className="details">
+              <div className="colorName">{`이름 : ${colorCard.name}`}</div>
+              <div className="colorRgb">{`rgb : ${colorCard.rgb}`}</div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
+
+const COLOR_CARD = [
+  { name: "red", rgb: "255,0,0" },
+  { name: "yellow", rgb: "255,255,0" },
+  { name: "green", rgb: "0,128,0" },
+  { name: "blue", rgb: "0,0,255" },
+];
 
 export default Introduce;
