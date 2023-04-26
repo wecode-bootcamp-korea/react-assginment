d182a18a74277ccd2f25afd456cc32cafb6f4a3b
yoohmoon <ansekfzhd5455@naver.com>
2023-04-26 09:15:56 +0900

    Assignment4-4 : productList 페이지 mock Data 구현

--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -1,67 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ProductList.scss";
 
 const ProductList = () => {
+  const [catList, setCatList] = useState([]);
+
+  useEffect(() => {
+    fetch("/data/catInfo.json")
+      .then((response) => response.json())
+      .then((data) => {
+        setCatList(data);
+      });
+  }, []);
+
+  console.log("catList ", catList);
+  // 왜 맨 처음에 빈배열이 찍히는데, 렌더링 상에는 에러가 발생하지 않지???
+
   return (
     <div className="productList">
       <h2 className="title">고양이 리스트</h2>
       <div className="listContainer">
-        <div className="listBox">
-          <img className="listImage" src="/images/cat.jpeg" alt="cat" />
-          <span className="listTitle">고양이</span>
-          <span>나이 : 4살</span>
-          <span>성별 : male</span>
-          <span>거주지 : 춘천</span>
-        </div>
-        <div className="listBox">
-          <img className="listImage" src="/images/cat.jpeg" alt="cat" />
-          <span className="listTitle">고냥이</span>
-          <span>나이 : 14살</span>
-          <span>성별 : female</span>
-          <span>거주지 : 양평</span>
-        </div>
-        <div className="listBox">
-          <img className="listImage" src="/images/cat.jpeg" alt="cat" />
-          <span className="listTitle">고용이</span>
-          <span>나이 : 7살</span>
-          <span>성별 : male</span>
-          <span>거주지 : 천안</span>
-        </div>
-        <div className="listBox">
-          <img className="listImage" src="/images/cat.jpeg" alt="cat" />
-          <span className="listTitle">모울이</span>
-          <span>나이 : 1살</span>
-          <span>성별 : female</span>
-          <span>거주지 : 부산</span>
-        </div>
-        <div className="listBox">
-          <img className="listImage" src="/images/cat.jpeg" alt="cat" />
-          <span className="listTitle">곰율이</span>
-          <span>나이 : 3살</span>
-          <span>성별 : male</span>
-          <span>거주지 : 노원</span>
-        </div>
-        <div className="listBox">
-          <img className="listImage" src="/images/cat.jpeg" alt="cat" />
-          <span className="listTitle">고울이</span>
-          <span>나이 : 11살</span>
-          <span>성별 : male</span>
-          <span>거주지 : 평양</span>
-        </div>
-        <div className="listBox">
-          <img className="listImage" src="/images/cat.jpeg" alt="cat" />
-          <span className="listTitle">고물이</span>
-          <span>나이 : 9살</span>
-          <span>성별 : male</span>
-          <span>거주지 : 전주</span>
-        </div>
-        <div className="listBox">
-          <img className="listImage" src="/images/cat.jpeg" alt="cat" />
-          <span className="listTitle">기양이</span>
-          <span>나이 : 2살</span>
-          <span>성별 : male</span>
-          <span>거주지 : 충청</span>
-        </div>
+        {catList.map((cat) => {
+          return (
+            <div key={cat.id} className="listBox">
+              <img className="listImage" alt="cat" src={cat.img} />
+              <span className="listTitle">{cat.name}</span>
+              <span>나이 : {cat.age}살</span>
+              <span>성별 : {cat.gender}</span>
+              <span>거주지 : {cat.city}</span>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
