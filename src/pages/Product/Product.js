df3225cf53c721127cbc3fd66c91f543f1ecddbf
오승민 <seungmin@oseungmin-ui-noteubug.local>
2023-04-28 22:47:12 +0900

    이제 진짜겠지

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -11,7 +11,7 @@ const Product = () => {
         <div className="productDetailImg">
           <img
             src={`/images/golf-ball-.jpg`} // color 이름에 따라 다른 이미지 경로 넣기
-            alt={`golf-ball`}
+            alt={`golf-ball`} 
           />
           {/* ColorButton 컴포넌트 위치 */}
         </div>
