6602660230c5db79de32f3a2a76a18e1c223a995
오승민 <seungmin@oseungmin-ui-noteubug.local>
2023-04-30 22:49:40 +0900

    Assignment3-1 : Color 컴포넌트 구현

--- src/pages/Inotroduce/introduce.js
+++ src/pages/Inotroduce/introduce.js
@@ -1,10 +1,29 @@
 import React from "react";
 import "./Introduce.scss";
+import { Link , useNavigate } from "react-router-dom";
 
 
 const Introduce = () => {
+
+   const navigate = useNavigate();
+
+   const goToProduct = () => {
+      navigate('/product');
+   }
+
    return(
-    <div>김개발</div>
+      <>
+      <p>오승민</p>
+      <p>45기</p>
+      <input/>
+      <button onClick={goToProduct}>클릭</button>
+      <div>
+      <Link to ="/product">product 페이지로 이동</Link>
+      </div>
+      
+
+      </>
+      
    )
    
 }
