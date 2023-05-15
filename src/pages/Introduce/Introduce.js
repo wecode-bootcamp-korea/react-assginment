cb9c17192f38d2f3396a8a4667b24f4cc49a178d
zosuzin <zosuzin@gmail.com>
2023-05-15 21:45:49 +0900

    Assignment 1-2 : event 사용

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,6 +1,13 @@
 import React from "react";
 import "./Introduce.css";
 
+const handleInput = () => {
+  console.log("input 태그 동작");
+};
+const handleButton = () => {
+  console.log("button 태그 동작");
+};
+
 function Introduce() {
   return (
     <>
@@ -9,6 +16,10 @@ function Introduce() {
           <img alt="위코드로고" src="images/wecode.webp" />
           <p className="name">조수진</p>
           <p className="number">46기</p>
+          <div className="boxBottom">
+            <input onChange={handleInput}></input>
+            <button onClick={handleButton}>클릭</button>
+          </div>
         </div>
       </main>
     </>
