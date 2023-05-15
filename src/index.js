9c946b15910d1b0984e70d611f831f7d0bda885c
minjeong <hn01472@gmail.com>
2023-05-16 06:28:28 +0900

    Assignment1-2 : event 사용하기

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Introduce from "./pages/Introduce/Introduce";
+import "./pages/Introduce/Introduce.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render();
 root.render(<Introduce />);
--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,14 +1,28 @@
 import React from "react";
+import { flushSync } from "react-dom";
 
 import "./Introduce.css";
 
 function Introduce() {
+  function handleButton() {
+    console.log("button 태그 동작");
+  }
+
+  function handleInput() {
+    console.log("input 태그 동작");
+  }
+
   return (
     <div className="wecode">
       <div className="Wrap">
-        <div className="Image">>wecode</div>
+        <div className="image">>wecode</div>
         <p className="Name">김민정</p>
         <p className="Number">46기 </p>
+
+        <div className="btnWrap">
+          <input type={"Text"} onChange={handleInput} />
+          <button onClick={handleButton}>클릭</button>
+        </div>
       </div>
     </div>
   );
