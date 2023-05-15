01415b782f5ab816524f9b564a38c63003d416a7
yeonnnnn.k <yeonnnnn.k@gmail.com>
2023-05-15 20:56:49 +0900

    Assignment1-2: event 사용하기

--- src/pages/Introduce/introduce.js
+++ src/pages/Introduce/introduce.js
@@ -1,18 +1,29 @@
 import React from "react";
 import "./introduce.css";
 
+const handleInput = () => {
+  console.log("input 태그 동작");
+};
+const handleButton = () => {
+  console.log("button 태그 동작");
+};
+
 const Introduce = () => {
   return (
     <>
       <div id="wrap">
         <div id="Black">
-          <p> &gt; wecode </p>
+          <p id="wecode"> &gt; wecode </p>
         </div>
         <div id="profile">
           <span id="bold">김지연</span>
           <br />
           <span>46기</span>
+          <br />
         </div>
+
+        <input type="text" onChange={handleInput} />
+        <button onClick={handleButton}>클릭</button>
       </div>
     </>
   );
