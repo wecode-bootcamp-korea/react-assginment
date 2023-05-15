24c5cecdb30450d47c122976b29c0a2e8c5095dc
이재웅 <wodnd@jaeung-ui-MacBookPro.local>
2023-05-15 22:42:25 +0900

    Assignment1-2 : event 사용하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -2,6 +2,12 @@ import React from "react"
 import "./Introduce.css"
 
 function Introduce(){
+    function handleInput(){
+        console.log('input 태그 동작')
+    }
+    function handleButton(){
+        console.log('button 태그 동작')
+    }
     return(
       <div className="container">
         <div className="card-box">
@@ -10,6 +16,8 @@ function Introduce(){
             </div>
             <h4>이재웅</h4>
             <p>46기</p>
+            <input type="text" onChange={handleInput} />
+            <button onClick={handleButton}>클릭</button>
         </div>
       </div>    
     )
