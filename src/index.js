3234ab1e06850b71b21252b6a67a7048f88f7a02
yeonnnnn.k <yeonnnnn.k@gmail.com>
2023-05-15 20:38:43 +0900

    Assignment1-1: 자기소개 컴포넌트 구현

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Introduce from "./pages/Introduce/introduce";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+root.render(<Introduce />);
---
+++ src/pages/Introduce/introduce.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./introduce.css";
+
+const Introduce = () => {
+  return (
+    <>
+      <div id="wrap">
+        <div id="Black">
+          <p> &gt; wecode </p>
+        </div>
+        <div id="profile">
+          <span id="bold">김지연</span>
+          <br />
+          <span>46기</span>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Introduce;
