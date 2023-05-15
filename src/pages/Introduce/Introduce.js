ce57c72aaf7b9e29211ff4a69c2864f3c60d5dc7
AGNESBAEK <jw.agnes.baek@gmail.com>
2023-05-15 21:28:13 +0900

    Assignment1-2 : event 사용하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,11 +1,23 @@
 import "./Introduce.css";
 
+const handleInput = () => {
+  console.log("input 태그 동작");
+};
+
+const handleButton = () => {
+  console.log("button 태그 동작");
+};
+
 const Introduce = () => {
   return (
     <div className="introduce">
       <img src="images/avatar.jpg" alt="" />
       <p className="name">백지율</p>
       <p className="th">46기</p>
+      <div className="textInput">
+        <input type={"text"} onClick={handleInput} placeholder="입력"></input>
+        <button onClick={handleButton}>클릭</button>
+      </div>
     </div>
   );
 };
