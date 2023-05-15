29f5e6b4156d42ef9872a22c1f32ed3eb39a6fb9
Kris <123653529+pmpaca@users.noreply.github.com>
2023-05-15 20:54:25 +0900

    Assignment1-1 : 자기소개 컴포넌트 구현

---
+++ src/pages/Introduce/Introduce.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Introduce.css";
+
+function Introduce() {
+  return (
+    <div className="container">
+      <img className="profile-pic" src="https://cdn.pixabay.com/photo/2023/05/15/11/43/11-43-23-560_960_720.jpg" alt="profile picture"/>
+      <div className="name">조혜진</div>
+      <div className="batch">46기</div>
+    </div>
+  )
+};
+
+
+
+export default Introduce;
