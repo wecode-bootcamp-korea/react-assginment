d3305d5cd5bdf6209b783306dd1f2c9953ef483c
minjeong <hn01472@gmail.com>
2023-05-15 23:40:18 +0900

    Assignment1-1 : 자기소개 컴포넌트 구현

---
+++ src/pages/Introduce/Introduce.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./Introduce.css";
+
+function Introduce() {
+  return (
+    <div className="wecode">
+      <div className="Wrap">
+        <div className="Image">>wecode</div>
+        <p className="Name">김민정</p>
+        <p className="Number">46기 </p>
+      </div>
+    </div>
+  );
+}
+
+export default Introduce;
