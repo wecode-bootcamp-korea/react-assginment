1bede6c43a9cb17863698bda0c6e38da25bb2f19
이재웅 <wodnd@jaeung-ui-MacBookPro.local>
2023-05-15 22:34:49 +0900

    Assignment1-1 : 자기소개 컴포넌트 구현

---
+++ src/pages/Introduce/Introduce.js
@@ -0,0 +1,18 @@
+import React from "react"
+import "./Introduce.css"
+
+function Introduce(){
+    return(
+      <div className="container">
+        <div className="card-box">
+            <div className="wecode">
+                <p>>wecode</p>
+            </div>
+            <h4>이재웅</h4>
+            <p>46기</p>
+        </div>
+      </div>    
+    )
+}
+
+export default Introduce
