a26b5853271cf6b94e2355ac7f7d6c94e1cfab72
Kaylla Son <kaylla@Kayllas-MacBook-Air.local>
2023-05-15 22:51:30 +0900

    Assignment1-1 :  자기소개 컴포넌트 구현

---
+++ src/pages/Introduce/Introduce.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Introduce.css';
+
+function Introduce () {
+    return (
+        <>
+            <main>
+                <article>
+                    <img src="/images/wecode_logo.png" alt="wecode logo" className="logo"/>
+                    <p className="myName">손자현</p>
+                    <p className="description">46기</p>
+                </article>
+            </main>
+        </>
+    );
+};
+
+export default Introduce;
