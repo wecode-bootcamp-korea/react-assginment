7462e48c082810a56b363b261fdb039a2c202645
AGNESBAEK <jw.agnes.baek@gmail.com>
2023-05-15 20:02:09 +0900

    Assignment1-1 : 자기소개 컴포넌트 구현

---
+++ src/pages/Introduce/Introduce.js
@@ -0,0 +1,13 @@
+import "./Introduce.css";
+
+const Introduce = () => {
+  return (
+    <div className="introduce">
+      <img src="images/avatar.jpg" alt="" />
+      <p className="name">백지율</p>
+      <p className="th">46기</p>
+    </div>
+  );
+};
+
+export default Introduce;
