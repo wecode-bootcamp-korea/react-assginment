3b48aae4ca4913683f943496d7dc0c924fb93627
zosuzin <zosuzin@gmail.com>
2023-05-15 21:11:59 +0900

    Assignment1-1: 자기소개 컴포넌트 구현

---
+++ src/pages/Introduce/Introduce.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Introduce.css";
+
+function Introduce() {
+  return (
+    <>
+      <main>
+        <div className="box">
+          <img alt="위코드로고" src="images/wecode.webp" />
+          <p className="name">조수진</p>
+          <p className="number">46기</p>
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default Introduce;
