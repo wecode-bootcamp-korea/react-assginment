7c1bc25ed214550e44eaeee07b2fb2c31654a0f5
Kaylla Son <kaylla@Kayllas-MacBook-Air.local>
2023-05-15 23:17:26 +0900

    Assignment1-2 : event 사용하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -2,16 +2,30 @@ import React from 'react';
 import './Introduce.css';
 
 function Introduce () {
+    function handleInput() {
+        console.log("input 태그 동작");
+    }
+    
+    function handleButton() {
+        console.log("button 태그 동작");
+    }
+
     return (
-        <>
-            <main>
-                <article>
-                    <img src="/images/wecode_logo.png" alt="wecode logo" className="logo"/>
-                    <p className="myName">손자현</p>
-                    <p className="description">46기</p>
-                </article>
-            </main>
-        </>
+        <main>
+            <article>
+                <img src="/images/wecode_logo.png" alt="wecode logo" className="logo"/>
+                <p className="myName">손자현</p>
+                <p className="description">46기</p>
+                <div className="inputContainer">
+                    <input 
+                        type="text" 
+                        className="inputBox"
+                        onChange={event => handleInput()}
+                    />
+                    <button type="button" className="inputBtn" onClick={event => handleButton()}>클릭</button>
+                </div>
+            </article>
+        </main>
     );
 };
 
