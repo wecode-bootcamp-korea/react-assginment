61fde4d47586fc2aa472cfa34c43413f62bf2572
GUNWOONG JANG <gunwoong32@gmail.com>
2023-05-16 09:52:39 +0900

    Assignment1-2 : event 사용하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -10,10 +10,25 @@ const Introduce = () => {
           alt="프로필사진"
         />
         <p className="name">장건웅</p>
-        <p className="thYear">46기</p>
+        <p className="YearTh">46기</p>
+        <div className="form-wrap">
+          <input className="introduce-input" onChange={handleInput} />
+          <button
+            className="introduce-button"
+            value="버튼"
+            onClick={handleButton}
+          />
+        </div>
       </div>
     </main>
   );
+};
+
+const handleInput = () => {
+  console.log("input 태그 동작");
+};
+const handleButton = () => {
+  console.log("button 태그 동작");
 };
 
 export default Introduce;
