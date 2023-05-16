a829e504ce13f3a4f00e0231912116dd28671427
Kris <123653529+pmpaca@users.noreply.github.com>
2023-05-16 17:58:41 +0900

    Assignment2-2 : Sass 적용

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Introduce.css";
+import "./Introduce.scss";
 import { Link } from "react-router-dom";
 
 function Introduce() {
@@ -11,14 +11,16 @@ function Introduce() {
   }
 
   return (
-    <div className="container">
-      <img className="profile-pic" src="https://cdn.pixabay.com/photo/2023/05/15/11/43/11-43-23-560_960_720.jpg" alt="profile picture"/>
-      <div className="name">조혜진</div>
-      <div className="batch">46기</div>
-      <input type="text" onChange={handleInput} />
-      <Link to="/product"><button onClick={handleButton}>클릭</button></Link>
-      <br/>
-      <Link to="/product">product 페이지로 이동</Link>
+    <div className="introduce">
+      <div className="container">
+        <img className="profile-pic" src="https://cdn.pixabay.com/photo/2023/05/15/11/43/11-43-23-560_960_720.jpg" alt="profile picture"/>
+        <div className="name">조혜진</div>
+        <div className="batch">46기</div>
+        <input type="text" onChange={handleInput} />
+        <Link to="/product"><button onClick={handleButton}>클릭</button></Link>
+        <br/>
+        <Link to="/product">product 페이지로 이동</Link>
+      </div>
     </div>
   );
 };
