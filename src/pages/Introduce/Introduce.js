673467579dd6447300f81833edc0b0257808bd14
이재웅 <wodnd@jaeung-ui-MacBookPro.local>
2023-05-16 16:04:56 +0900

    Assignment2-1 : Router 컴포넌트 구현

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,12 +1,15 @@
 import React from "react"
+import { useNavigate,Link } from "react-router-dom"
 import "./Introduce.css"
 
 function Introduce(){
+    let navigate = useNavigate();
     function handleInput(){
         console.log('input 태그 동작')
     }
     function handleButton(){
         console.log('button 태그 동작')
+        navigate('/product')
     }
     return(
       <div className="container">
@@ -18,6 +21,9 @@ function Introduce(){
             <p>46기</p>
             <input type="text" onChange={handleInput} />
             <button onClick={handleButton}>클릭</button>
+            <div>
+            <Link to='/product'>product페이지로 이동</Link>
+            </div>
         </div>
       </div>    
     )
