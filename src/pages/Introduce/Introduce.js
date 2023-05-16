6ba8b92f4072e736d36b3e975f8bd25463e202bf
AGNESBAEK <jw.agnes.baek@gmail.com>
2023-05-16 12:08:58 +0900

    Assignment2-2 : Sass 적용

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import "./Introduce.css";
+import "./Introduce.scss";
 
 const handleInput = () => {
   console.log("input 태그 동작");
@@ -10,9 +10,9 @@ const Introduce = () => {
   const navigate = useNavigate();
   return (
     <div className="introduce">
-      <img src="images/avatar.jpg" alt="" />
+      <img src="images/avatar.jpg" alt="main" />
       <p className="name">백지율</p>
-      <p className="th">46기</p>
+      <p className="batch">46기</p>
       <div className="textInput">
         <input type={"text"} onClick={handleInput} placeholder="입력"></input>
         <button
