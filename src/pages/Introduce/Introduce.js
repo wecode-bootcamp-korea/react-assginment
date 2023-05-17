cffc8e0c6cbf5f37bc984d9392f6c444af95b867
shfkd123 <shfkd123@gmail.com>
2023-05-17 21:36:25 +0900

    FIX : Assignment2-2 Sass 기능 적용 수정

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,38 +1,39 @@
-import React from "react"
-import "./Introduce.scss";
+import React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import "./Introduce.scss";
 
+function Introduce() {
+  const navigate = useNavigate();
 
-function Introduce (){
-    const navigate = useNavigate();
-    
-    const handleButton = () => {
-        navigate('./product');
-    }
+  const handleButton = () => {
+    navigate("/product");
+  };
 
-    const handleInput = (e) => {
-        console.log("input 태그 동작");
-    }
-    return(
-        <div className="main">
-            <div className="container">
-                <div className="container-inner">
-                    <div className="user-box">
-                    <img className ="user-img" src="images/cat.jpeg" alt="user-img"/>
-                    </div>
-                    <div className="user-exp">
-                        <span className="boldStyle">김민지</span>
-                        <br/>
-                        <span className="smallStyle">46기</span>
-                        <br />
-                        <input type="text" onChange={handleInput}/>
-                        <button type="button" onClick={handleButton}>클릭</button>
-                        <Link to="./product">product페이지로 이동</Link>
-                    </div>
-                </div>
-            </div>
+  const handleInput = () => {
+    console.log("input 태그 동작");
+  };
+  return (
+    <div className="main">
+      <div className="container">
+        <div className="container-inner">
+          <div className="user-box">
+            <img className="user-img" src="images/cat.jpeg" alt="user-img" />
+          </div>
+          <div className="user-exp">
+            <span className="boldStyle">김민지</span>
+            <br />
+            <span className="smallStyle">46기</span>
+            <br />
+            <input className="inputs" type="text" onChange={handleInput} />
+            <button className="clickBtn" type="button" onClick={handleButton}>
+              클릭
+            </button>
+            <Link to="/product">product페이지로 이동</Link>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
 export default Introduce;
