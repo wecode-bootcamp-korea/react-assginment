b69ce89071665ccfded17bf8f0f00704bedd52bd
Maple <jinu@Mapleui-MacBook-Pro.local>
2023-05-17 18:27:21 +0900

    Assignment 2-1 : React Router : Router 컴포넌트 및 기능 구현하기 완료

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,16 +1,17 @@
 import React from "react";
 import titleImage from "../../images/images.png";
+import { Link, useNavigate } from "react-router-dom";
 import "./Introduce.css";
 
-const handleButton = () => {
-  console.log("button 태그 동작");
-};
-
-const handleInput = () => {
-  console.log("input 태그 동작");
-};
-
 const Introduce = () => {
+  const navigate = useNavigate();
+  function handleInput() {
+    console.log("input 태그 동작");
+  }
+  function handleButton() {
+    console.log("button 태그 동작");
+    navigate("/product");
+  }
   return (
     <div className="introduce">
       <img src={titleImage} alt="main" className="mainImage" />
@@ -20,6 +21,7 @@ const Introduce = () => {
         <input type="text" onChange={handleInput} />
         <button onClick={handleButton}>클릭</button>
       </div>
+      <Link to="/product">product 페이지로 이동</Link>
     </div>
   );
 };
