617a460d8794d142be9f58d094d6ea3d61f9c26b
GUNWOONG JANG <gunwoong32@gmail.com>
2023-05-18 01:24:00 +0900

    Assignment3-1 : Color 컴포넌트 구현

--- src/pages/Product/components/Color/Color.js
+++ src/pages/Product/components/Color/Color.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Color.scss";
 
 const Color = () => {
+  const [color, setColor] = useState("white");
+  const [text, setText] = useState("white");
+
   return (
     <div className="color">
       <span className="colorText">
         색상 :
-        <div className="selected" />
-        white
+        <div className="selected" style={{ backgroundColor: color }} />
+        {text}
       </span>
       <div className="colorHandler">
-        <button className="colorButton white" onClick={() => {}} />
-        <button className="colorButton red" onClick={() => {}} />
-        <button className="colorButton yellow" onClick={() => {}} />
+        <button
+          className="colorButton white"
+          onClick={() => {
+            setColor("white");
+            setText("white");
+          }}
+        />
+        <button
+          className="colorButton red"
+          onClick={() => {
+            setColor("red");
+            setText("red");
+          }}
+        />
+        <button
+          className="colorButton yellow"
+          onClick={() => {
+            setColor("yellow");
+            setText("yellow");
+          }}
+        />
       </div>
     </div>
   );
