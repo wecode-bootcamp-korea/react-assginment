c85aec73811136e4082005b53c182f00dddc4f57
AGNESBAEK <jw.agnes.baek@gmail.com>
2023-05-18 18:42:27 +0900

    MODIFY: Assignment3-3 : Review 컴포넌트 구현 (state명 수정)

--- src/pages/Product/components/Review/Review.js
+++ src/pages/Product/components/Review/Review.js
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import "./Review.scss";
 
 const Review = () => {
-  const [review, setReview] = useState(true);
+  const [ifReview, setIfReview] = useState(true);
 
-  const onClick = () => setReview((prev) => !prev);
+  const onClick = () => setIfReview(!ifReview);
 
   return (
     <div className="review">
@@ -17,7 +17,7 @@ const Review = () => {
       </div>
       <div className="reviewTitle">배송도 빠르고 정말 좋아요.</div>
       <div className="reviewMain">
-        {!review && (
+        {!ifReview && (
           <div className="reviewMainContents">
             주문한지 하루만에 배송이 도착해서 너무 좋았습니다.
             <br />
@@ -25,7 +25,7 @@ const Review = () => {
           </div>
         )}
         <span className="reviewMainHandler" onClick={onClick}>
-          {review ? "더보기 ▼" : "접기 ▲"}
+          {ifReview ? "더보기 ▼" : "접기 ▲"}
         </span>
       </div>
     </div>
