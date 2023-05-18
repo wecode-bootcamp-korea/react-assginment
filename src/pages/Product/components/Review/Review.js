e669505f30d4b58088c0f049330cac56f21d2c0d
이재웅 <wodnd@jaeung-ui-MacBookPro.local>
2023-05-18 16:54:50 +0900

    Assignment3-3 : Review 컴포넌트 구현

--- src/pages/Product/components/Review/Review.js
+++ src/pages/Product/components/Review/Review.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Review.scss";
 
+let count = 0 ;
 const Review = () => {
+  let [review,setReview] = useState(false);
+  let [btn,setBtn] = useState('더보기 ▼');
   return (
     <div className="review">
       <div className="user">
@@ -13,17 +16,38 @@ const Review = () => {
       </div>
       <div className="reviewTitle">배송도 빠르고 정말 좋아요.</div>
       <div className="reviewMain">
-        <div className="reviewMainContents">
-          주문한지 하루만에 배송이 도착해서 너무 좋았습니다.
-          <br />
-          다음에도 필요하면 또 여기서 시킬 것 같아요.
-        </div>
-        <span className="reviewMainHandler" onClick={() => {}}>
-          더보기 ▼
+        {
+          review === true ? <ReviewMainContents/> : null
+        }
+        {/* // <div className="reviewMainContents">
+        //   주문한지 하루만에 배송이 도착해서 너무 좋았습니다.
+        //   <br />
+        //   다음에도 필요하면 또 여기서 시킬 것 같아요.
+        // </div> */}
+        <span className="reviewMainHandler" onClick={() => {
+          setReview(!review);
+          count++;
+          if(count%2===1){
+            setBtn('접기 ▲')
+          }else{
+            setBtn('더보기 ▼')
+          }
+        }}>
+          {btn}
         </span>
       </div>
     </div>
   );
 };
+
+const ReviewMainContents = () => {
+  return(
+    <div className="reviewMainContents">
+          주문한지 하루만에 배송이 도착해서 너무 좋았습니다.
+          <br />
+          다음에도 필요하면 또 여기서 시킬 것 같아요.
+    </div>
+  )
+}
 
 export default Review;
