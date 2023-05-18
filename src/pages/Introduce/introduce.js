77c8e318bcaab2aab8902b4b6ed82f674a6f6e75
yeonnnnn.k <yeonnnnn.k@gmail.com>
2023-05-18 12:22:43 +0900

    FIX: 리뷰 수정 반영

--- src/pages/Introduce/introduce.js
+++ src/pages/Introduce/introduce.js
@@ -1,34 +1,38 @@
 import React from "react";
 import "./introduce.scss";
-import { Link } from "react-router-dom";
-
-const handleInput = () => {
-  console.log("input 태그 동작");
-};
-const handleButton = () => {
-  console.log("button 태그 동작");
-};
+import { Link, useNavigate } from "react-router-dom";
 
 const Introduce = () => {
+  const navigate = useNavigate();
+  const handleButton = () => {
+    console.log("button 태그 동작");
+    navigate("/product");
+  };
+  const handleInput = () => {
+    console.log("input 태그 동작");
+  };
+
   return (
     <>
       <div className="wrap">
         <div className="profileBackgroundColor">
-          <p id="wecode"> &gt; wecode </p>
+          <p className="wecode"> &gt; wecode </p>
         </div>
-        <div id="profile">
-          <span id="bold">김지연</span>
+        <div className="profile">
+          <span className="bold">김지연</span>
           <br />
           <span>46기</span>
           <br />
         </div>
 
-        <input type="text" onChange={handleInput} />
+        <input className="inputText" type="text" onChange={handleInput} />
+
+        <button className="clickBtn" onClick={handleButton}>
+          클릭
+        </button>
+
         <Link to="Product">
-          <button onClick={handleButton}>클릭</button>
-        </Link>
-        <Link to="Product">
-          <a href="/">product 페이지로 이동</a>
+          <span className="moveToProductPage">product 페이지로 이동</span>
         </Link>
       </div>
     </>
