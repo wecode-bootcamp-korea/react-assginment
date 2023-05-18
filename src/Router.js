31ccc28828b2be0a3ca583d8cb4b4e2ba9001ac2
Maple <jinu@Mapleui-MacBook-Pro.local>
2023-05-18 17:18:13 +0900

    Router.js 수정완료

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,12 @@
 import React from "react";
 
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Introduce from "./pages/Introduce/Introduce";
 import Product from "./pages/Product/Product";
 import ProductList from "./pages/ProductList/ProductList";
 import Review from "./pages/Product/components/Review/Review";
 import Count from "./pages/Product/components/Count/Count";
 import Color from "./pages/Product/components/Color/Color";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const Router = () => {
   return (
