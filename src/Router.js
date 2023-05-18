c8156f0bcfb4455a66ac4d6a26d7a39ae9c56af8
이재웅 <wodnd@jaeung-ui-MacBookPro.local>
2023-05-18 15:53:15 +0900

    Assignment 2-1,2-2: 리뷰 수정 반영

--- src/Router.js
+++ src/Router.js
@@ -1,25 +1,26 @@
-import React from "react"
-import { BrowserRouter,Routes,Route } from "react-router-dom"
-import Introduce from "./pages/Introduce/Introduce"
-import Product from "./pages/Product/Product"
-import ProductList from "./pages/ProductList/ProductList"
-// import Review from ""
-// import Count from ""
-// import Color from ""
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Introduce from "./pages/Introduce/Introduce";
+import Product from "./pages/Product/Product";
+import ProductList from "./pages/ProductList/ProductList";
+import Review from "./pages/Product/components/Review/Review";
+import Count from "./pages/Product/components/Count/Count";
+import Color from "./pages/Product/components/Color/Color";
+
 
 const Router = () => {
-    return(
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Introduce/>} />
-                <Route path="/product" element={<Product/>} />
-                <Route path="/productList" element={<ProductList/>} />
-                {/* <Route path="/review" element={<Review/>} />
-                <Route path="/count" element={<Count/>} />
-                <Route path="/color" element={<Color/>} /> */}
-            </Routes>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Introduce />} />
+        <Route path="/product" element={<Product />} />
+        <Route path="/productList" element={<ProductList />} />
+        <Route path="/review" element={<Review/>} />
+        <Route path="/count" element={<Count/>} />
+        <Route path="/color" element={<Color/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default Router
+export default Router;
--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,34 +1,32 @@
-import React from "react"
-import { useNavigate,Link } from "react-router-dom"
-import "./Introduce.scss"
-import "../../style/variables.scss"
+import React from "react";
+import { useNavigate, Link } from "react-router-dom";
+import "./Introduce.scss";
 
-
-function Introduce(){
-    let navigate = useNavigate();
-    function handleInput(){
-        console.log('input 태그 동작')
-    }
-    function handleButton(){
-        console.log('button 태그 동작')
-        navigate('/product')
-    }
-    return(
-      <div className="container">
-        <div className="card-box">
-            <div className="wecode">
-                <p>>wecode</p>
-            </div>
-            <h4>이재웅</h4>
-            <p>46기</p>
-            <input type="text" onChange={handleInput} />
-            <button onClick={handleButton}>클릭</button>
-            <div>
-            <Link to='/product'>product페이지로 이동</Link>
-            </div>
+function Introduce() {
+  let navigate = useNavigate();
+  function handleInput() {
+    console.log("input 태그 동작");
+  }
+  function handleButton() {
+    console.log("button 태그 동작");
+    navigate("/product");
+  }
+  return (
+    <div className="container">
+      <div className="card-box">
+        <div className="wecode">
+          <p>>wecode</p>
         </div>
-      </div>    
-    )
+        <h4 className="name">이재웅</h4>
+        <p>46기</p>
+        <input type="text" onChange={handleInput} />
+        <button onClick={handleButton}>클릭</button>
+        <div>
+          <Link to="/product">product페이지로 이동</Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Introduce
+export default Introduce;
