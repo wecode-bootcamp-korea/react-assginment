08fe8b3dfd90435cf3079b2ea7397498317f029d
Kris <123653529+pmpaca@users.noreply.github.com>
2023-05-19 10:39:42 +0900

    Assignment3-3 : Review 컴포넌트 구현

--- src/pages/Product/components/Review/Review.js
+++ src/pages/Product/components/Review/Review.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Review.scss";
 
 const Review = () => {
+  const [toggleOn, setToggleOn] = useState(false);
+  const [toggleButton, setToggleButton] = useState("더보기 ▼");
+  const buttonChange = () => {
+    setToggleButton("접기 ▲");
+  };
   return (
     <div className="review">
       <div className="user">
@@ -13,13 +18,21 @@ const Review = () => {
       </div>
       <div className="reviewTitle">배송도 빠르고 정말 좋아요.</div>
       <div className="reviewMain">
-        <div className="reviewMainContents">
-          주문한지 하루만에 배송이 도착해서 너무 좋았습니다.
-          <br />
-          다음에도 필요하면 또 여기서 시킬 것 같아요.
-        </div>
-        <span className="reviewMainHandler" onClick={() => {}}>
-          더보기 ▼
+        {toggleOn && (
+          <div className="reviewMainContents">
+            주문한지 하루만에 배송이 도착해서 너무 좋았습니다.
+            <br />
+            다음에도 필요하면 또 여기서 시킬 것 같아요.
+          </div>
+        )}
+        <span
+          className="reviewMainHandler"
+          onClick={() => {
+            setToggleOn(!toggleOn);
+            buttonChange();
+          }}
+        >
+          {toggleButton}
         </span>
       </div>
     </div>
