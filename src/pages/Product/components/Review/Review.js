36987f65153cd3132e671021550a66ad46a157bb
shfkd123 <shfkd123@gmail.com>
2023-05-21 22:11:20 +0900

    FIX : Review 컴포넌트 조건부 렌더링으로 구현

--- src/pages/Product/components/Review/Review.js
+++ src/pages/Product/components/Review/Review.js
@@ -2,24 +2,7 @@ import React, { useState } from "react";
 import "./Review.scss";
 
 const Review = () => {
-  const [direction, setDirection] = useState(true);
-
-  const [review, setReview] = useState([
-    {
-      id: "1",
-      content: "주문한지 하루만에 배송이 도착해서 너무 좋았습니다.",
-    },
-    {
-      id: "2",
-      content: "다음에도 필요하면 또 여기서 시킬 것 같아요.",
-    },
-  ]);
-
-  const handleReview = () => {
-    setDirection(!direction);
-  };
-
-  const directionReview = direction ? review : [];
+  const [review, setReview] = useState(false);
   return (
     <div className="review">
       <div className="user">
@@ -31,18 +14,20 @@ const Review = () => {
       </div>
       <div className="reviewTitle">배송도 빠르고 정말 좋아요.</div>
       <div className="reviewMain">
-        <div className="reviewMainContents">
-          {directionReview.map((review, id) => {
-            return (
-              <span key={id}>
-                {review.content}
-                <br />
-              </span>
-            );
-          })}
-        </div>
-        <span className="reviewMainHandler" onClick={handleReview}>
-          {direction ? "더보기 ▼" : "접기 ^"}
+        {!review && (
+          <div className="reviewMainContents">
+            주문한지 하루만에 배송이 도착해서 너무 좋았습니다.
+            <br />
+            다음에도 필요하면 또 여기서 시킬 것 같아요.
+          </div>
+        )}
+        <span
+          className="reviewMainHandler"
+          onClick={() => {
+            setReview(!review);
+          }}
+        >
+          {review ? "더보기 ▼" : "접기 ^"}
         </span>
       </div>
     </div>
