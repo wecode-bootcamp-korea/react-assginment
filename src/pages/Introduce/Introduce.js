1ebd7bebbc3621da9a69088254ff0ea9d1edcc0b
이재웅 <wodnd@jaeung-ui-MacBookPro.local>
2023-05-22 23:01:51 +0900

    Assignment4-2 : ColorCard 상수데이터로 구현하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -15,21 +15,39 @@ function Introduce() {
   }
 
   return (
-    <div className="introduce">
-      <div className="card-box">
-        <div className="wecode">
-          <p>>wecode</p>
-        </div>
-        <h4 className="name">이재웅</h4>
-        <p>46기</p>
-        <input type="text" onChange={handleInput} />
-        <button onClick={handleButton}>클릭</button>
-        <div>
-          <Link to="/product">product페이지로 이동</Link>
+    <>
+      <div className="introduce">
+        <div className="card-box">
+          <div className="wecode">
+            <p>>wecode</p>
+          </div>
+          <h4 className="name">이재웅</h4>
+          <p>46기</p>
+          <input type="text" onChange={handleInput} />
+          <button onClick={handleButton}>클릭</button>
+          <div>
+            <Link to="/product">product페이지로 이동</Link>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="introduce">
+        {CARD_INFO_LIST.map((info) => (
+          <div className="card">
+            <div className={`color ${info.className}`}></div>
+            <h4 className="name">이름 : {info.name}</h4>
+            <p>rgb : {info.rgb}</p>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
 export default Introduce;
+
+const CARD_INFO_LIST = [
+  { id: 1, className: "red", name: "red", rgb: "255,0,0" },
+  { id: 2, className: "yellow", name: "yellow", rgb: "255,255,0" },
+  { id: 3, className: "green", name: "green", rgb: "0,128,0" },
+  { id: 4, className: "blue", name: "blue", rgb: "0,0,255" }
+];
