7d4530362d53fa28f6f7e23172d71470651d74cb
AGNESBAEK <jw.agnes.baek@gmail.com>
2023-05-22 21:16:37 +0900

    Assignment4-2 : ColorCard 상수데이터로 구현하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -15,24 +15,44 @@ const Introduce = () => {
   };
 
   return (
-    <div className="introduce">
-      <img className="mainImg" src="images/avatar.jpg" alt="main" />
-      <p className="name">백지율</p>
-      <p className="batch">46기</p>
-      <div className="inputBox">
-        <input
-          className="textInput"
-          type="text"
-          onChange={handleInput}
-          placeholder="입력"
-        />
-        <button className="clickBtn" onClick={handleButton}>
-          클릭
-        </button>
+    <>
+      <div className="introduce">
+        <img className="mainImg" src="images/avatar.jpg" alt="main" />
+        <p className="name">백지율</p>
+        <p className="batch">46기</p>
+        <div className="inputBox">
+          <input
+            className="textInput"
+            type="text"
+            onChange={handleInput}
+            placeholder="입력"
+          />
+          <button className="clickBtn" onClick={handleButton}>
+            클릭
+          </button>
+        </div>
+        <Link to="/product">product 페이지로 이동</Link>
       </div>
-      <Link to="/product">product 페이지로 이동</Link>
-    </div>
+      <div className="color-card-list">
+        {COLOR_CARD_INFO.map((card) => {
+          return (
+            <div className="color-card" key={card.id}>
+              <div className={`color-card-background ${card.name}`}></div>
+              <div>이름: {card.name}</div>
+              <div>rgb: {card.rgb}</div>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
 export default Introduce;
+
+const COLOR_CARD_INFO = [
+  { id: 0, name: "red", rgb: "255, 0, 0" },
+  { id: 1, name: "yellow", rgb: "255, 255, 0" },
+  { id: 2, name: "green", rgb: "0, 128 , 0" },
+  { id: 3, name: "blue", rgb: "0, 0, 255" },
+];
