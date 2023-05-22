fc5a51d9b314acbfca0a38bfc0770df8dd1dca47
minjeong <hn01472@gmail.com>
2023-05-22 20:42:51 +0900

    boolean 값 수정

--- src/pages/Product/components/Review/Review.js
+++ src/pages/Product/components/Review/Review.js
@@ -15,13 +15,13 @@ const Review = () => {
       </div>
       <div className="reviewTitle">배송도 빠르고 정말 좋아요.</div>
       <div className="reviewMain">
-        {isMore === true ? (
+        {isMore && (
           <div className="reviewMainContents">
             주문한지 하루만에 배송이 도착해서 너무 좋았습니다.
             <br />
             다음에도 필요하면 또 여기서 시킬 것 같아요.
           </div>
-        ) : null}
+        )}
         <span
           className="reviewMainHandler"
           onClick={() => {
