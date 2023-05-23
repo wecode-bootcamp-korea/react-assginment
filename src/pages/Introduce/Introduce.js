65ed9617bae88f7632966983ade3b00ac093c5ba
Kris <123653529+pmpaca@users.noreply.github.com>
2023-05-23 15:24:45 +0900

    Assignment4-2 : ColorCard 상수데이터로 구현하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -4,19 +4,23 @@ import "./Introduce.scss";
 
 function Introduce() {
   const navigate = useNavigate();
-
   const handleInput = () => {
     console.log("input 태그 동작");
   };
-
   const handleButton = () => {
     navigate("/product");
     console.log("button 태그 동작");
   };
+  const COLOR_LIST = [
+    { id: "red", colorName: "red", colorCode: "255, 0, 0" },
+    { id: "yellow", colorName: "yellow", colorCode: "255, 255, 0" },
+    { id: "green", colorName: "green", colorCode: "0, 128, 0" },
+    { id: "blue", colorName: "blue", colorCode: "0, 0, 255" },
+  ];
 
   return (
     <div className="introduce">
-      <div className="container">
+      <div className="profile-container">
         <img
           className="profile-pic"
           src="./images/sydney.jpg"
@@ -30,6 +34,17 @@ function Introduce() {
         <Link to="/product" className="toProductPage">
           product 페이지로 이동
         </Link>
+      </div>
+      <div className="color-card-container">
+        {COLOR_LIST.map((colorList) => {
+          return (
+            <div className="color-card">
+              <div className={`color-box ${colorList.id}`}></div>
+              <div className="color-name">이름 : {colorList.colorName} </div>
+              <div className="color-code">rgb : {colorList.colorCode}</div>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
