c3e1e009f5501c2a405e1d23ac4a6eee6f458812
minjeong <hn01472@gmail.com>
2023-05-23 12:11:46 +0900

    Assignment4-4 : productList 페이지 mock Data 구현

--- src/pages/ProductList/ProductList.js
+++ src/pages/ProductList/ProductList.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./ProductList.scss";
 
 const ProductList = () => {
+  const [catInfo, setCatInfo] = useState([]);
+
+  useEffect(() => {
+    fetch("/data/catInfoList.json")
+      .then((Response) => Response.json())
+      .then((result) => setCatInfo(result));
+  }, []);
+
   return (
     <div className="productList">
       <h2 className="title">고양이 리스트</h2>
       <div className="listContainer">
-        <div className="listBox">
+        {catInfo.map((catInfo) => {
+          return (
+            <div className="listBox">
+              <img className="listImage" src="/images/cat.jpeg" alt="cat" />
+              <span className="listTitle">{catInfo.name}</span>
+              <span>나이 : {catInfo.age}</span>
+              <span>성별 : {catInfo.gender}</span>
+              <span>거주지 : {catInfo.residence}</span>
+            </div>
+          );
+        })}
+        {/* <div className="listBox">
           <img className="listImage" src="/images/cat.jpeg" alt="cat" />
           <span className="listTitle">고양이</span>
           <span>나이 : 4살</span>
@@ -61,7 +80,7 @@ const ProductList = () => {
           <span>나이 : 2살</span>
           <span>성별 : male</span>
           <span>거주지 : 충청</span>
-        </div>
+        </div> */}
       </div>
     </div>
   );
