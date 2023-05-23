9eb0afda29a48ed5126a69d169c31070bedb2203
Kris <123653529+pmpaca@users.noreply.github.com>
2023-05-23 17:40:59 +0900

    Assignment4-3 : product 페이지 mock Data 구현

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Color from "./components/Color/Color";
 import ColorButton from "./components/ColorButton/ColorButton";
 import Count from "./components/Count/Count";
@@ -7,32 +7,37 @@ import "./Product.scss";
 
 const Product = () => {
   const [colorName, setColorName] = useState("white");
-
   const [countNumber, setCountNumber] = useState(1);
+  const [productData, setProductData] = useState({});
+  const price = productData.price;
+  const totalPrice = (price * countNumber).toLocaleString();
 
-  const price = 300;
-  const totalPrice = price * countNumber;
+  useEffect(() => {
+    fetch("/data/productData.json")
+      .then((response) => response.json())
+      .then((result) => setProductData(result));
+  }, []);
 
   return (
     <div className="product">
       <div className="productDetail">
         <div className="productDetailImg">
           <img
-            src={`/images/golf-ball-${colorName}.jpg`} // color 이름에 따라 다른 이미지 경로 넣기
+            src={`${productData.img}${colorName}.jpg`} // color 이름에 따라 다른 이미지 경로 넣기
             alt={`golf-ball`}
           />
           <ColorButton colorName={colorName} />
         </div>
         <div className="productDetailInfo">
-          <span className="title">골프공</span>
+          <span className="title">{productData.title}</span>
           <span>비거리를 비약적으로 늘려줍니다</span>
-          <span>가격 : {price.toLocaleString()} 원</span>
+          <span>가격 : {price} 원</span>
           <Color colorName={colorName} setColorName={setColorName} />
           <div className="quantity">
             <span> 수량 : </span>
             <Count countNumber={countNumber} setCountNumber={setCountNumber} />
           </div>
-          <span>최종 가격 : {totalPrice.toLocaleString()} 원</span>
+          <span>최종 가격 : {totalPrice} 원</span>
           <button className="buyBtn">구매하기</button>
         </div>
       </div>
@@ -40,7 +45,7 @@ const Product = () => {
         <div className="reviewListHeader">
           <span>상품평</span>
         </div>
-        <Review />
+        <Review review={productData.review} />
       </div>
     </div>
   );
