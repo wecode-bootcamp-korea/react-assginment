5f00513cb6847407aa7a597c306b830469babcae
AGNESBAEK <jw.agnes.baek@gmail.com>
2023-05-24 11:47:27 +0900

    MODIFY: 리뷰 수정 반영

--- src/pages/Product/Product.js
+++ src/pages/Product/Product.js
@@ -8,7 +8,7 @@ import "./Product.scss";
 const Product = () => {
   const [color, setColor] = useState("white");
   const [count, setCount] = useState(1);
-  const [productList, setProductList] = useState([]);
+  const [productList, setProductList] = useState({});
 
   const price = 300;
   const totalPrice = price * count;
@@ -22,37 +22,32 @@ const Product = () => {
   }, []);
 
   return (
-    productList.length &&
-    productList.map((product) => {
-      return (
-        <div className="product" key={product.id}>
-          <div className="productDetail">
-            <div className="productDetailImg">
-              <img src={`/images/golf-ball-${color}.jpg`} alt={`golf-ball`} />
-              <ColorButton color={color} setColor={setColor} />
-            </div>
-            <div className="productDetailInfo">
-              <span className="title">{product.title}</span>
-              <span>비거리를 비약적으로 늘려줍니다</span>
-              <span>가격 : {price.toLocaleString()} 원</span>
-              <Color color={color} setColor={setColor} />
-              <div className="quantity">
-                <span> 수량 : </span>
-                <Count count={count} onChange={setCount} />
-              </div>
-              <span>최종 가격 : {totalPrice.toLocaleString()} 원</span>
-              <button className="buyBtn">구매하기</button>
-            </div>
-          </div>
-          <div className="reviewList">
-            <div className="reviewListHeader">
-              <span>상품평</span>
-            </div>
-            <Review review={product.review} />
-          </div>
+    <div className="product" key={productList.id}>
+      <div className="productDetail">
+        <div className="productDetailImg">
+          <img src={`/images/golf-ball-${color}.jpg`} alt={`golf-ball`} />
+          <ColorButton color={color} setColor={setColor} />
         </div>
-      );
-    })
+        <div className="productDetailInfo">
+          <span className="title">{productList.title}</span>
+          <span>비거리를 비약적으로 늘려줍니다</span>
+          <span>가격 : {price.toLocaleString()} 원</span>
+          <Color color={color} setColor={setColor} />
+          <div className="quantity">
+            <span> 수량 : </span>
+            <Count count={count} onChange={setCount} />
+          </div>
+          <span>최종 가격 : {totalPrice.toLocaleString()} 원</span>
+          <button className="buyBtn">구매하기</button>
+        </div>
+      </div>
+      <div className="reviewList">
+        <div className="reviewListHeader">
+          <span>상품평</span>
+        </div>
+        <Review review={productList.review} />
+      </div>
+    </div>
   );
 };
 
