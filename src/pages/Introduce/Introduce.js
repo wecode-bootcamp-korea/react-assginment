b62b0fd826d24be1eb1a3cea307d7f30aacf44f8
이원준 <wonjun@MacBook-Pro-2.local>
2023-05-24 19:16:17 +0900

    1-1

---
+++ src/pages/Introduce/Introduce.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Introduce.scss" ;
+
+const Introduce = () => {
+return (
+<div className="outBox">
+    <div className="inBox">
+         <div className="wecode">>wecode </div>
+    </div>
+    <div className="texts">
+        <div className="name">이원준</div>
+        <div className="num">45기</div>
+    </div>
+    </div>
+
+    );
+};
+
+export default Introduce ;
