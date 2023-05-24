9a431ad966b4598a46b5bd8521579b3341150aeb
이원준 <wonjun@MacBook-Pro-2.local>
2023-05-24 19:58:15 +0900

    1-2

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -2,6 +2,14 @@ import React from "react";
 import "./Introduce.scss" ;
 
 const Introduce = () => {
+    const handleInput = () => {
+        return console.log( "input 태그 동작")
+    }
+
+    const handleButton = () => {
+        return console.log(  "button 태그 동작")
+    }
+
 return (
 <div className="outBox">
     <div className="inBox">
@@ -10,6 +18,10 @@ return (
     <div className="texts">
         <div className="name">이원준</div>
         <div className="num">45기</div>
+    </div>
+    <div>
+        <input onChange={handleInput}/> 
+        <button onClick={handleButton}>클릭 </button>
     </div>
     </div>
 
