8488e8425ee0aefcfc7da93502ca9a25d7b53c5c
양회진 <yanghoejin@yanghoejin-ui-MacBookAir.local>
2023-06-12 20:46:08 +0900

    Assignment1-2 : event 사용하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Introduce.css";
 
 function MissionOne() {
+  const [InputValue, inputTag] = useState("");
+  const [ButtonValue, buttonTag] = useState("");
+
+  function HandleInput(e) {
+    inputTag(e.target.value);
+    console.log("input 태그 동작");
+  }
+
+  function HandleButton(e) {
+    buttonTag(e.target.value);
+    console.log("button 태그 동작");
+  }
   return (
     <>
       <div className="container">
@@ -11,6 +23,12 @@ function MissionOne() {
         <div className="letters">
           <p className="name">양회진</p>
           <p className="number">47기</p>
+        </div>
+        <div className="inputButton">
+          <input id="input" value={InputValue} onChange={HandleInput}></input>
+          <button id="button" value={ButtonValue} onClick={HandleButton}>
+            클릭
+          </button>
         </div>
       </div>
     </>
