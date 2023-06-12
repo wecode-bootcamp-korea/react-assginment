737288f4b33deede394d415aa2dfb17601ca327b
Kimsu <betnwjd25@gmail.com>
2023-06-12 17:38:46 +0900

    Assignment1-1 : 자기소개 컴포넌트 구현

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Introduce from "./introduce";
+import "./styles/introduce.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+root.render(<Introduce />);
---
+++ src/introduce.js
@@ -0,0 +1,17 @@
+const Introduce = () => {
+  return (
+    <div className="profile">
+      <div className="profile-box">
+        <img
+          className="profile-image"
+          alt="catPhoto"
+          src="images/profile.png"
+        ></img>
+        <h3 className="user-name">김수정</h3>
+        <h4 className="class-number">47기</h4>
+      </div>
+    </div>
+  );
+};
+
+export default Introduce;
