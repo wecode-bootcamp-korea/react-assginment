bf7832d364e95a696dc3c1dc603ece8642429838
양회진 <yanghoejin@yanghoejin-ui-MacBookAir.local>
2023-06-12 19:00:04 +0900

    Assignment1-1 : 자기소개 컴포넌트 구현

---
+++ src/pages/Introduce/Introduce.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Introduce.css";
+
+function MissionOne() {
+  return (
+    <>
+      <div className="container">
+        <div className="photo">
+          <img alt="hambbang" src="images/IMG_7632.JPG" />
+        </div>
+        <div className="letters">
+          <p className="name">양회진</p>
+          <p className="number">47기</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default MissionOne;
