b5c27015adda4b9debb85577ec08245f169d19af
Kimsu <betnwjd25@gmail.com>
2023-06-12 19:36:31 +0900

    Assignment1-2 : event 사용하기

--- src/introduce.js
+++ src/introduce.js
@@ -1,4 +1,10 @@
 const Introduce = () => {
+  const handleButton = () => {
+    console.log("button tag 동작");
+  };
+  const changeInput = () => {
+    console.log("input value 변동");
+  };
   return (
     <div className="profile">
       <div className="profile-box">
@@ -9,6 +15,12 @@ const Introduce = () => {
         ></img>
         <h3 className="user-name">김수정</h3>
         <h4 className="class-number">47기</h4>
+        <div>
+          <input className="write-input" onChange={changeInput}></input>
+          <button className="click-button" onClick={handleButton}>
+            클릭
+          </button>
+        </div>
       </div>
     </div>
   );
