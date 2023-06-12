93a0f394d8390b71563f22d8360417d0613c8a3b
Kimsu <betnwjd25@gmail.com>
2023-06-12 19:59:36 +0900

    Assignment1-2 : event 사용하기

---
+++ src/pages/Product/components/Color/Color.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "./Color.scss";
+
+const Color = () => {
+  return (
+    <div className="color">
+      <span className="colorText">
+        색상 :
+        <div className="selected" />
+        white
+      </span>
+      <div className="colorHandler">
+        <button className="colorButton white" onClick={() => {}} />
+        <button className="colorButton red" onClick={() => {}} />
+        <button className="colorButton yellow" onClick={() => {}} />
+      </div>
+    </div>
+  );
+};
+
+export default Color;
