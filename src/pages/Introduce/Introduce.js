e6e012e511fb4c2564a45f0c60e4a5c07452ec7e
양회진 <yanghoejin@yanghoejin-ui-MacBookAir.local>
2023-06-13 17:35:41 +0900

    Assignment2-2 : Sass 적용

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-import "./Introduce.css";
+import "./Introduce.scss";
 
-function MissionOne() {
+function Introduce() {
   const navigate = useNavigate();
 
   function HandleInput() {
@@ -14,26 +14,22 @@ function MissionOne() {
     navigate("/Product");
   }
   return (
-    <>
-      <div className="container">
-        <div className="photo">
-          <img alt="hambbang" src="images/IMG_7632.JPG" />
-        </div>
-        <div className="letters">
-          <p className="name">양회진</p>
-          <p className="number">47기</p>
-        </div>
-        <div className="inputButton">
-          <input id="input" onChange={HandleInput}></input>
-          <button id="button" onClick={HandleButton}>
-            클릭
-          </button>
-          <br />
-          <Link to="./Product">move to Product page</Link>
-        </div>
+    <div className="introduce">
+      <img className="photo" alt="hambbang" src="images/IMG_7632.JPG" />
+      <div className="letters">
+        <p className="name">양회진</p>
+        <p className="number">47기</p>
       </div>
-    </>
+      <div className="inputButton">
+        <input id="input" onChange={HandleInput}></input>
+        <button id="button" onClick={HandleButton}>
+          클릭
+        </button>
+        <br />
+        <Link to="./Product">move to Product page</Link>
+      </div>
+    </div>
   );
 }
 
-export default MissionOne;
+export default Introduce;
