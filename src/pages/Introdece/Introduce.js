cef50a535bd18264a09ccd2179a6e3f92a82ca1f
최진이 <jini.choi@choejin-iui-MacBookAir.local>
2023-06-13 12:18:35 +0900

    MODIFY : introduce className 수정

--- src/pages/Introdece/Introduce.js
+++ src/pages/Introdece/Introduce.js
@@ -1,29 +1,35 @@
-import './introduce.css'
+import "./Introduce.css";
 
 const Introduce = () => {
-   const handleInput = () => {
-     console.log('input 태그 동작');
-   }
+  const handleInput = () => {
+    console.log("input 태그 동작");
+  };
 
-   const handleButton = (event) => {
+  const handleButton = (event) => {
     event.preventDefault();
-    console.log('button 태그 동작');
-  }
+    console.log("button 태그 동작");
+  };
 
-    return <>
-        <div className='introduceContainer'>
-            <div className='introduceBox'>
-                <img className='introduceImg' src='/images/jini.jpg' alt='jini' />
-                <span className='introduceName'>최진이</span>
-                <span className='introduceNum'>47기</span>
-                <form>
-                    <input className='introduceInput' type='text' placeholder='내용입력' onChange={handleInput}/>
-                    <button className='introduceBtn' type='sumbit' onClick={handleButton}>클릭</button>
-                </form>
-            </div>
-        </div>
-        
-    </>;    
+  return (
+    <div className="introduce">
+      <div className="introduceBox">
+        <img className="introduceImg" src="/images/jini.jpg" alt="jini" />
+        <span className="introduceName">최진이</span>
+        <span className="introduceNum">47기</span>
+        <form>
+          <input
+            className="introduceInput"
+            type="text"
+            placeholder="내용입력"
+            onChange={handleInput}
+          />
+          <button className="introduceBtn" type="sumbit" onClick={handleButton}>
+            클릭
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Introduce;
