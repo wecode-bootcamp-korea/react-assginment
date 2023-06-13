daef3a6a765b7dc0b00c6e900c82fc516ec1a0c8
이지원 <manz19@ijiwon-ui-MacBookAir.local>
2023-06-14 00:44:49 +0900

    Assignment1-2 : event 사용하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,18 +1,26 @@
-import React from "react"
-import "./Introduce.css"
+import React from "react";
+import "./Introduce.css";
 
 function Introduce() {
-    return (
-        <div className="introduce">
-            <div className="img">
-                <img className="wecodeImg" src="images/wecode.png" alt="wecodeLogo" />
-                </div>
-            <div className="text">
-                <p className="name">이지원</p>
-                <p className="num">47기</p>
-            </div>
-        </div>
-    );
+  const handleInput = () => {
+    console.log("input 태그 동작");
+  };
+
+  const handleButton = () => {
+    console.log("button 태그 동작");
+  };
+
+  return (
+    <div className="introduce">
+      <img className="wecodeImg" src="images/wecode.png" alt="wecodeLogo" />
+      <div className="text">
+        <p className="name">이지원</p>
+        <p className="num">47기</p>
+        <input className="input" type="text" placeholder="입력" onChange={handleInput} />
+        <button className="btn" onClick={handleButton}>Click</button>
+      </div>
+    </div>
+  );
 }
 
-export default Introduce
+export default Introduce;
