e99ff049f79facf909987efdcd93b75e3fce64bb
이지원 <manz19@ijiwon-ui-MacBookAir.local>
2023-06-14 00:29:12 +0900

    Assignment1-1 : 자기소개 컴포넌트 구현

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -4,7 +4,9 @@ import "./Introduce.css"
 function Introduce() {
     return (
         <div className="introduce">
-            <img className="wecodeImg" src="images/wecode.png" alt="wecodeLogo" />
+            <div className="img">
+                <img className="wecodeImg" src="images/wecode.png" alt="wecodeLogo" />
+                </div>
             <div className="text">
                 <p className="name">이지원</p>
                 <p className="num">47기</p>
