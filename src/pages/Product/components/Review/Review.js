3778cc67d3da817de8cb63e98d69f58d5bba11b6
최진이 <jini.choi@choejin-iui-MacBookAir.local>
2023-06-15 17:53:07 +0900

    Assignment3-3 : Review 컴포넌트 구현

--- src/pages/Product/components/Review/Review.js
+++ src/pages/Product/components/Review/Review.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Review.scss";
 
 const Review = () => {
+  const [review, setReview] = useState(true);
+  const [display, setDisplay] = useState(true);
+
+  const more = () => {
+    setReview(!review);
+    setDisplay(!display);
+  };
   return (
     <div className="review">
       <div className="user">
@@ -13,13 +20,21 @@ const Review = () => {
       </div>
       <div className="reviewTitle">배송도 빠르고 정말 좋아요.</div>
       <div className="reviewMain">
-        <div className="reviewMainContents">
+        <div
+          className="reviewMainContents"
+          style={{ display: `${display ? "block" : "none"}` }}
+        >
           주문한지 하루만에 배송이 도착해서 너무 좋았습니다.
           <br />
           다음에도 필요하면 또 여기서 시킬 것 같아요.
         </div>
-        <span className="reviewMainHandler" onClick={() => {}}>
-          더보기 ▼
+        <span
+          className="reviewMainHandler"
+          onClick={() => {
+            more();
+          }}
+        >
+          {review ? "접기 ▲" : "더보기 ▼"}
         </span>
       </div>
     </div>
