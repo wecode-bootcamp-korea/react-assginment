969ae4599d493335733ec7bf1df59951295f058d
이지원 <manz19@ijiwon-ui-MacBookAir.local>
2023-06-16 00:37:06 +0900

    Assignment3-3 : Review 컴포넌트 구현

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "./Router";
-//import Product from "./pages/Product/Product";
-//import Introduce from "./pages/Introduce/Introduce";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Router />);
