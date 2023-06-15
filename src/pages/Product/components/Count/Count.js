d8b687bc265b18158824e8a8d6e1af548275f6dc
양회진 <yanghoejin@yanghoejin-ui-MacBookAir.local>
2023-06-15 18:55:04 +0900

    Assignment 3-2 구현 완료

--- src/pages/Product/components/Count/Count.js
+++ src/pages/Product/components/Count/Count.js
@@ -1,15 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Count.scss";
 
 const Count = () => {
+  const [number, setNumber] = useState(1);
+
+  function upNumber() {
+    setNumber(number + 1);
+  }
+
+  function downNumber() {
+    if (number > 1) {
+      setNumber(number - 1);
+    }
+  }
+
+  function reset() {
+    setNumber(1);
+  }
+
   return (
     <div className="count">
       <div className="countInput">
-        <button onClick={() => {}}>-</button>
-        <div className="countInputText">1</div>
-        <button onClick={() => {}}>+</button>
+        <button
+          onClick={() => {
+            downNumber(number);
+          }}
+        >
+          -
+        </button>
+        <div className="countInputText">{number}</div>
+        <button
+          onClick={() => {
+            upNumber(number);
+          }}
+        >
+          +
+        </button>
       </div>
-      <button className="resetBtn" onClick={() => {}}>
+      <button
+        className="resetBtn"
+        onClick={() => {
+          reset(number);
+        }}
+      >
         reset
       </button>
     </div>
