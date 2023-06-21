f77969a47be343fec07bbd0bd724a0365eb3acaf
최진이 <jini.choi@choejin-iui-MacBookAir.local>
2023-06-21 17:21:47 +0900

    Assignment4-1 : menu List 상수데이터로 구현

--- src/pages/components/Nav/Nav.js
+++ src/pages/components/Nav/Nav.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+// import {MENU_LIST} from
 import "./Nav.scss";
 
 const Nav = () => {
@@ -22,14 +23,22 @@ const Nav = () => {
       </div>
       {isOpenMenu && (
         <div className="menuBox">
-          <Link to="/">메인으로 가기</Link>
-          <Link to="/review">리뷰 컴포넌트 가기</Link>
-          <Link to="/count">카운트 컴포넌트 가기</Link>
-          <Link to="/color">컬러 컴포넌트 가기</Link>
+          {MENU_LIST.map((info) => (
+            <Link to={info.to} key={info.id}>
+              {info.text}
+            </Link>
+          ))}
         </div>
       )}
     </div>
   );
 };
+
+const MENU_LIST = [
+  { id: 1, to: "/", text: "메인으로 가기" },
+  { id: 2, to: "/review", text: "리뷰 컴포넌트 가기" },
+  { id: 3, to: "/count", text: "카운트 컴포넌트 가기" },
+  { id: 4, to: "/color", text: "컬러 컴포넌트 가기" },
+];
 
 export default Nav;
