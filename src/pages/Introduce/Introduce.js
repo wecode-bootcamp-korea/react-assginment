d73949d1c73ddecca4c55e47395757836b17379c
yoonl <70797985+5yoonl@users.noreply.github.com>
2023-08-27 21:12:31 +0900

    ADD : Introduce component

---
+++ src/pages/Introduce/Introduce.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Introduce =()=>{
+    return(
+        <div>
+            자기소개 페이지 입니다.
+            <br />
+            과제안내에 따라 UI를 그려주세요
+        </div>
+    )
+}
+
+export default Introduce
