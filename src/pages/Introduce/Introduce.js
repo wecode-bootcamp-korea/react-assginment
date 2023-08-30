92c1676fb824f6161143082d09ab5a616be2f706
Minjae Park <jayy.intheworld@gmail.com>
2023-08-30 23:08:46 +0900

    Assignment1-1 : 자기소개 컴포넌트 구현 완성

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import "./Introduce.css";
+import capture from "../../assets/images/1692785007289.jpg";
+
 
 const Introduce =()=>{
     return(
-        <div>
-            자기소개 페이지 입니다.
-            <br />
-            과제안내에 따라 UI를 그려주세요
+        <div className="main">
+            <div className="introBox">
+                <div className="imageBox">
+                    <img src={capture} alt="profile" />
+                </div>
+                <div className="textBox">
+                    <strong className="name">박민재</strong>
+                    <span className="semester">49기</span>
+                </div>
+            </div>
         </div>
     )
 }
+
 
 export default Introduce
