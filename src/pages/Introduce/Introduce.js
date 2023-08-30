6f124a942666b22fd3ad0404d2985ccfaac34edb
jyj <dudwns5013@naver.com>
2023-08-30 16:41:08 +0900

    Assignment1-1 : 자기소개 컴포넌트 구현

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React from "react";
+import "./Introduce.css";
+import logo from "../Introduce/wecode.png";
 
-const Introduce =()=>{
-    return(
-        <div>
-            자기소개 페이지 입니다.
-            <br />
-            과제안내에 따라 UI를 그려주세요
+const Introduce = () => {
+  return (
+    <div className="introduce">
+      <div className="introduce-container">
+        <img className="wecode-logo" src={logo} alt="wecode"></img>
+        <div className="introduce-container-body">
+          <div>조영준</div>
+          <div className="generation">49기</div>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Introduce
+export default Introduce;
