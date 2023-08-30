76139807d75f77e8645cf9fa5d3458c639b7f4c8
philamones <philamones@icloud.com>
2023-08-30 22:48:56 +0900

    [Assignment 1-2] React intro : event 사용하기 완성

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -13,9 +13,21 @@ const Introduce =()=>{
                     <strong>류창선</strong>
                     <span>49기</span>
                 </div>
+                <div>
+                    <input type="text" onChange={handleInput} />
+                    <button type="button" onClick={handleButton}>클릭</button>
+                </div>
             </section>
         </div>
     )
+}
+
+function handleInput () {
+    console.log("input 태그 동작");
+}
+
+function handleButton () {
+    console.log("button 태그 동작");
 }
 
 export default Introduce
