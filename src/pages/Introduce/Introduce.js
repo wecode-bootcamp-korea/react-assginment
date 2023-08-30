05083e667551769af9cff3a8dd3674c25593ad28
이주은 <leeje7766@gmail.com>
2023-08-30 23:21:30 +0900

    [Assignment 1-1] React intro : 자기소개 컴포넌트 구현하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import "./Introduce.css";
 
 const Introduce =()=>{
     return(
-        <div>
-            자기소개 페이지 입니다.
-            <br />
-            과제안내에 따라 UI를 그려주세요
+        <div className="flex">
+        <section className="introduce-item">
+            <div>
+                <img className="image" src="images/1000.jpg" alt="내사진" />
+            </div>
+            <div>
+                <span className="name">이주은</span>
+            </div>
+            <div>
+                <span className="number">49기</span>
+            </div>
+            </section>
         </div>
     )
 }
