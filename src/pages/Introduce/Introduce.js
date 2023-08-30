220ae08ed9687a251048e7cdf315f962cb2cc909
jongwan Kim <aegis0918@naver.com>
2023-08-30 18:45:22 +0900

    assignment 1-1 complete

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React from "react";
+import "./Introduce.css";
+import image_picture from "../../img/ko_592_1.jpg";
 
-const Introduce =()=>{
-    return(
-        <div>
-            자기소개 페이지 입니다.
-            <br />
-            과제안내에 따라 UI를 그려주세요
+const Introduce = () => {
+  return (
+    <div className="container">
+      <div className="main">
+        <div className="main_image">
+          <img src={image_picture} alt="hoshino_ai" className="image"></img>
         </div>
-    )
-}
+        <div className="main_text">
+          <span className="user_nick">개쩌는 개발자</span>
+          <span className="user_name">49기 김종완</span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Introduce
+export default Introduce;
