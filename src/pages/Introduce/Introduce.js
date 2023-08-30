6cf292c51c81c3dd85baf3b350e72bca62610206
jyj <dudwns5013@naver.com>
2023-08-30 21:56:50 +0900

    [49기 조영준] Assignment 1-2

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,16 +1,43 @@
 import React from "react";
-import "./Introduce.css";
 import logo from "../Introduce/wecode.png";
+import "./Introduce.css";
+
+const Info = () => {
+  return (
+    <div className="introduce-container-info">
+      <strong>조영준</strong>
+      <div className="generation">49기</div>
+    </div>
+  );
+};
+
+const Form = () => {
+  return (
+    <form className="introduce-container-form">
+      <input
+        onChange={() => {
+          console.log("input 태그 동작");
+        }}
+      ></input>
+      <input
+        type="button"
+        value="클릭
+          "
+        onClick={() => {
+          console.log("button 태그 동작");
+        }}
+      />
+    </form>
+  );
+};
 
 const Introduce = () => {
   return (
     <div className="introduce">
       <div className="introduce-container">
         <img className="wecode-logo" src={logo} alt="wecode"></img>
-        <div className="introduce-container-body">
-          <div>조영준</div>
-          <div className="generation">49기</div>
-        </div>
+        <Info />
+        <Form />
       </div>
     </div>
   );
