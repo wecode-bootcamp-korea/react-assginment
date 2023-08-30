3c8f53cea0fba745bebe828307840b774637fcc0
coolllleooo <buseness200824@gmail.com>
2023-08-30 21:41:48 +0900

    add introduce.js

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import "./introduce.css";
+
 
 const Introduce =()=>{
+    const imageUrl = "/images/golf-ball-white.jpg" // public 폴더 하위의 이미지 경로
     return(
+        <div className="fullline">
         <div>
-            자기소개 페이지 입니다.
-            <br />
-            과제안내에 따라 UI를 그려주세요
-        </div>
+            <img className="thumbnail" src={imageUrl} alt="cat"></img>
+      </div>
+      </div>
     )
 }
 
