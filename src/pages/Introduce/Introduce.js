751542f355ca73b865813b1a6f4eb809b21bbfc2
박인국 <pkook7@gmail.com>
2023-08-30 18:29:54 +0900

    Assignment1-2 : event 사용하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -2,6 +2,16 @@ import React from 'react';
 import './Introduce.scss';
 
 const Introduce =()=>{
+
+
+    const handleInput = () => {
+            console.log("input 태그 동작")
+        }
+
+     const handleButton = () => {
+            console.log("button 태그 동작")
+        }
+
     return(
         <React.Fragment>
             <div className="bigBox">
@@ -10,6 +20,10 @@ const Introduce =()=>{
                 </div>
                 <div className='name'>박인국</div>
                 <div className='number'>49기</div>
+                <div className='inputBox'>
+                    <input type="text" className="input" onChange={handleInput}/>
+                    <button className='button' onClick={handleButton}>클릭</button>
+                </div>
             </div>    
         </React.Fragment>
     )
