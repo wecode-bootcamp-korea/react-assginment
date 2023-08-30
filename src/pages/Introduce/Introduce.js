2d0d57e2f73533dd7c95197995d2a1e52607c0d2
sseyeon2 <sseyoun2@gmail.com>
2023-08-30 16:42:02 +0900

    assignment1-1 : 자기소개 컴포넌트 구현

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React from "react";
+import "./style.css";
 
-const Introduce =()=>{
-    return(
-        <div>
-            자기소개 페이지 입니다.
-            <br />
-            과제안내에 따라 UI를 그려주세요
-        </div>
-    )
-}
+const Introduce = () => {
+    return (
+        <>
+            <div className="intro-wrapper">
+                <div className="intro-box">
+                    <p className="img-box">
+                        <img src="/images/cat.jpeg" alt="사진" />
+                    </p>
+                    <p className="name">김세연</p>
+                    <span>49기</span>
+                </div>
+            </div>
+        </>
+    );
+};
 
-export default Introduce
+export default Introduce;
