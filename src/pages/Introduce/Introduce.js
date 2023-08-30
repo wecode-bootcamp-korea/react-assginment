3235adac2ef2abdb64ff453e4825f79259f0a0f1
KYEONGMIN CHO <rlarldud8987@naver.com>
2023-08-30 21:57:10 +0900

    Assignment1-1 : 자기소개 컴포넌트 구현

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import "./Introduece.css";
 
 const Introduce =()=>{
     return(
-        <div>
-            자기소개 페이지 입니다.
-            <br />
-            과제안내에 따라 UI를 그려주세요
+        <div className="introduce">
+           <div className="container">
+           <img className="main_img" src="./images/golf-ball-red.jpg" alt="사진없음"/>
+            <p className="name">김기영</p>
+            <p className="group">49기</p>
+            </div>
         </div>
     )
 }
