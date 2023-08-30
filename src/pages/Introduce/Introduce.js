4360de314f729a86fc03433b43dfe72846711b2c
권기완 <digh7169@gmail.com>
2023-08-30 16:21:30 +0900

    Assignment1-1 : 자기소개 컴포넌트 구현

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import "./Introduce.scss"
 
 const Introduce =()=>{
     return(
-        <div>
-            자기소개 페이지 입니다.
-            <br />
-            과제안내에 따라 UI를 그려주세요
+        <div className='introduceContainer'>   
+            <div className='centerBox'>
+                <div className="introduceBox">
+                    <img src = "/images/cat.jpeg"/>
+                    <div>
+                        <p className='name'>권기완</p>
+                        <p className='wecodNum'>49기</p>
+                    </div>
+                </div>
+            </div>
+            
         </div>
     )
 }
