2d846e67f542f70dd2bcfca52940d0263dabe732
sylee <soyeun.lee5930@gmail.com>
2023-08-30 23:13:01 +0900

    Assignment1-2 : event 사용하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -3,6 +3,14 @@ import "./Introduce.css";
 import wecodelogo from "./wecodelogo.png";
 
 const Introduce = () => {
+  const handleInput = () => {
+    console.log("input 태그 동작");
+  };
+
+  const handleButton = () => {
+    console.log("button 태그 동작");
+  };
+
   return (
     <div className="introBody">
       <div className="container">
@@ -11,6 +19,12 @@ const Introduce = () => {
           <div className="textDiv">
             <p className="name">이소연</p>
             <p className="th">49기</p>
+          </div>
+          <div className="inputDiv">
+            <input className="inputText" onChange={handleInput} type="text" />
+            <button className="inputBtn" onClick={handleButton}>
+              클릭
+            </button>
           </div>
         </div>
       </div>
