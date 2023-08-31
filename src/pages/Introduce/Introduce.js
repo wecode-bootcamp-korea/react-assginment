2f2f8f2b5e5bc2981ca8e9772d62fd7ddd05a6e7
박인국 <pkook7@gmail.com>
2023-08-31 20:48:48 +0900

    Assignment2-1 : Router 컴포넌트 구현

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,29 +1,36 @@
 import React from 'react';
 import './Introduce.scss';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Introduce =()=>{
-
 
     const handleInput = () => {
             console.log("input 태그 동작")
         }
 
-     const handleButton = () => {
-            console.log("button 태그 동작")
-        }
+    //  const handleButton = () => {
+    //         console.log("button 태그 동작")
+    //     }
+
+    const navigate = useNavigate();
+
+    const navigateProduct = () => {
+            navigate('/product')
+    }
 
     return(
         <React.Fragment>
             <div className="bigBox">
                 <div className="pictureBox">
-                    <img src={`/images/다람쥐.jpg`} alt="myPicture"/>
+                    <img src={'/images/다람쥐.jpg'} alt="myPicture"/>
                 </div>
                 <div className='name'>박인국</div>
                 <div className='number'>49기</div>
                 <div className='inputBox'>
                     <input type="text" className="input" onChange={handleInput}/>
-                    <button className='button' onClick={handleButton}>클릭</button>
+                    <button className='button' onClick={navigateProduct}>클릭</button>
                 </div>
+                <Link to='/product'>product 페이지로 이동</Link>
             </div>    
         </React.Fragment>
     )
