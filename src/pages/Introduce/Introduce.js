2874046f694430cb8823ba86d22a697b0c97a0c7
Leechengwon <leecwee8497@gmail.com>
2023-08-31 19:40:45 +0900

    [49기 이청원] Assignment 1-2

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -2,15 +2,25 @@ import React from 'react';
 import './Introduce.css'
 
 const Introduce =()=>{
+    function handleInput(){
+        console.log( "input 태그 동작")
+    }
+    function handleButton(){
+        console.log("button 태그 동작")
+     }
     return(
-        
+    
         <div className='Bigbox'>
             <div className='Myimg'>
                  <img src='자기소개.jpeg' />  
             </div>
             <div className='Name'>
                 <h1>이청원</h1> 
-                <p>49기</p>
+                <p className='Cardinal'>49기</p>
+            </div>
+            <div className='Input'>
+                <input onChange={handleInput} type='text'></input>
+                <button onClick={handleButton}>클릭</button>
             </div>
         </div>
     
