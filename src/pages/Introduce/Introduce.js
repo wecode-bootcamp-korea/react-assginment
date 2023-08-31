a5e04b983217348cf6782b92dc92706f9e2a16fe
coolllleooo <buseness200824@gmail.com>
2023-08-31 10:11:44 +0900

    [49기-박지원]assignment1

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -2,14 +2,40 @@ import React from 'react';
 import "./introduce.css";
 
 
-const Introduce =()=>{
-    const imageUrl = "/images/golf-ball-white.jpg" // public 폴더 하위의 이미지 경로
-    return(
+const Introduce = () => {
+    const imageUrl = "/images/assignment.jpg" // public 폴더 하위의 이미지 경로
+    function handleInput(){
+        console.log("input 태그 동작");
+    }
+     function handleButton(){
+        console.log("button 태그 동작");
+    }
+    return (
         <div className="fullline">
-        <div>
-            <img className="thumbnail" src={imageUrl} alt="cat"></img>
-      </div>
-      </div>
+            <div>
+                <img className="thumbnail" src={imageUrl} alt="cat"></img>
+            </div>
+            <h1 className="name">
+                박지원
+            </h1>
+
+            <h2>49기</h2>
+           
+    
+
+            <div className="Assignment2">
+                <input className="click" type="text" name="message" placeholder="텍스트를 입력하세요"
+                   onChange={handleInput}
+                
+                />
+
+           
+            <button onClick={handleButton}>클릭</button>
+
+            </div>
+
+
+        </div>
     )
 }
 
