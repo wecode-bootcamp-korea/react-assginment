1899937b286946e8c8d1e87bece324c579b4793d
jongwan Kim <aegis0918@naver.com>
2023-08-31 12:58:00 +0900

    assignment 1-2 complete

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -2,6 +2,13 @@ import React from "react";
 import "./Introduce.css";
 import image_picture from "../../img/ko_592_1.jpg";
 
+const handleInput = () => {
+  console.log("input 태그 동작");
+};
+const handleButton = () => {
+  console.log("button 태그 동작");
+};
+
 const Introduce = () => {
   return (
     <div className="container">
@@ -12,6 +19,12 @@ const Introduce = () => {
         <div className="main_text">
           <span className="user_nick">개쩌는 개발자</span>
           <span className="user_name">49기 김종완</span>
+        </div>
+        <div className="main_input_box">
+          <input className="profile_input" onChange={handleInput}></input>
+          <button className="profile_button" onClick={handleButton}>
+            검색
+          </button>
         </div>
       </div>
     </div>
