c34970f31d1359de857610a848f52111245c3da9
Minjae Park <jayy.intheworld@gmail.com>
2023-08-31 19:23:17 +0900

    Assignment 1-2 React intro : event 사용하기 완료

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -4,6 +4,14 @@ import capture from "../../assets/images/1692785007289.jpg";
 
 
 const Introduce =()=>{
+
+    function handleInput(){
+        console.log("input 태그 동작")
+    }
+    function handleButton(){
+        console.log("button 태그 동작")
+    }
+
     return(
         <div className="main">
             <div className="introBox">
@@ -14,10 +22,16 @@ const Introduce =()=>{
                     <strong className="name">박민재</strong>
                     <span className="semester">49기</span>
                 </div>
+                <section> 
+                    <input type="text" onChange={handleInput} />
+                    <button type="button" onClick={handleButton}>클릭</button>
+                </section>
             </div>
         </div>
     )
 }
+
+
 
 
 export default Introduce
