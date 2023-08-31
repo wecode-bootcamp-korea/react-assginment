0c36f2b56aa0f0e10f4569d1302f9c4990c6cca5
이주은 <leeje7766@gmail.com>
2023-08-31 21:08:08 +0900

    Assignment 1-2 : event 사용하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -7,30 +7,28 @@ const Introduce =()=>{
         console.log("input 태그 동작");
     }
     function handleButton(){
-        console.log("button 태그 동작")
+        console.log("button 태그 동작");
     }
 
     return(
         <div className="flex">
-        <section className="introduce-item">
-            <div>
-                <img className="image" src="images/1000.jpg" alt="내사진" />
-            </div>
-            <div>
-                <span className="name">이주은</span>
-            </div>
-            <div>
-                <span className="number">49기</span>
-            </div>
-            <div>
-                <input type="text" onChange={handleInput}/>
-            </div>
-            <div>
-                <button type="click" onClick={handleButton}>클릭</button>
-            </div>
-            <div>
-            </div>
-        </section>
+            <section className="introduce-item">
+                <div>
+                    <img className="image" src="images/1000.jpg" alt="내사진" />
+                </div>
+                <div>
+                    <span className="name">이주은</span>
+                </div>
+                <div>
+                    <span className="number">49기</span>
+                </div>
+                <div>
+                    <input type="text" onChange={handleInput}/>
+                </div>
+                <div>
+                    <button type="button" onClick={handleButton}>클릭</button>
+                </div>
+            </section>
         </div>
     )
 }
