1580751aede0e2a8f35503806a067681a5d2658f
이영은 <hisenselee12@gmail.com>
2023-08-31 16:20:19 +0900

    [Assignment 2-1] React Router : Router 컴포넌트 및 기능 구현하기 완성

---
+++ src/Router.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Introduce from './pages/Introduce/Introduce';
+import Product from './pages/Product/Product';
+
+const Router=()=>{
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Introduce/>} />
+                <Route path='/Introduce' element={<Introduce/>} />
+                <Route path='/Product' element={<Product/>} />
+
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default Router;
--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import "./introduceStyle.css";
+import "./introduceStyle.scss";
 import imgProfile from "./49-youngeun-profile.jpg";
+import { Link, useNavigate } from 'react-router-dom';
 
 
 const Introduce =()=>{
 
     const handleInput =() =>{
 
-        return console.log('input 태그 동작')
+        return console.log('input 태그 동작');
     }
 
-    const handleButton =() =>{
+    // const handleButton =() =>{
 
-        return console.log('button 태그 동작')
+    //     return console.log('button 태그 동작');
+    // }
+
+    const navigate = useNavigate();
+
+    const goToProduct=()=>{
+        navigate('/Product');
     }
 
 
     return(
-        <div className='wrap'>
-            <section className='imgProfile'><img src={imgProfile} alt='49-이영은의 프로필 사진'/></section>
-            <section>
+        <div className='introduce'>
+            <section className='imgProfile'><img className='profileImg' src={imgProfile} alt='49-이영은의 프로필 사진'/></section>
+            <section className='textName'>
             <h1>이영은</h1>
             <span>49기</span>
             </section>
@@ -33,11 +40,13 @@ const Introduce =()=>{
             type='button'
             className='clickBtn'
             value="클릭"
-            onClick={handleButton}
+            // onClick={handleButton}
+            onClick={goToProduct}
             />
             </section>
+            <p><Link to="/Product">product페이지로 이동</Link></p>
         </div>
-    )
-}
+    );
+};
 
-export default Introduce
+export default Introduce;
