533e7b8f68ad78385f2e97d392f8a029171367b9
안병우 <byungwoo1802@gmail.com>
2023-08-31 16:41:18 +0900

    Assignment1-2 : event 사용하기

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,6 +1,13 @@
 import React from "react";
 import "./introduce.css";
 const Introduce = () => {
+  const handClick = () => {
+    console.log("input 태그 동작");
+  };
+
+  const handButton = () => {
+    console.log("button 태그 동작");
+  };
   return (
     <div className="introduction">
       <div className="container">
@@ -13,6 +20,12 @@ const Introduce = () => {
           <div className="info">
             <p className="name">안병우</p>
             <p className="number">49기</p>
+          </div>
+          <div className="title">
+            <input type="text" className="textbox" onChange={handClick} />
+            <button className="click" onClick={handButton}>
+              클릭
+            </button>
           </div>
         </div>
       </div>
