edcb95fd020f4226d53a179d5a0951e57a35c138
안병우 <byungwoo1802@gmail.com>
2023-09-01 15:20:49 +0900

    ADD : Assignment2-2 Sass기능 적용하기

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,10 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Introduce from "./pages/Introduce/Introduce";
 import Product from "./pages/Product/Product";
+import ProductList from "./pages/ProductList/ProductList";
+import Review from "./pages/Product/components/Review/Review";
+import Count from "./pages/Product/components/Count/Count";
+import Color from "./pages/Product/components/Color/Color";
 
 const Router = () => {
   return (
@@ -9,6 +13,10 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Introduce />} />
         <Route path="/product" element={<Product />} />
+        <Route path="/productlist" element={<ProductList />} />
+        <Route path="/review" element={<Review />} />
+        <Route path="/count" element={<Count />} />
+        <Route path="/color" element={<Color />} />
       </Routes>
     </BrowserRouter>
   );
