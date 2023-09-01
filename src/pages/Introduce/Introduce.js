81ea6dac25eb11cce0f98bcd4ecc2ec7c5195205
Leechengwon <leecwee8497@gmail.com>
2023-09-01 17:40:44 +0900

    [49기 이청원] Assignment 2-2

--- src/pages/Introduce/Introduce.js
+++ src/pages/Introduce/Introduce.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './Introduce.css'
+import './Introduce.scss'
 import { Link,useNavigate } from 'react-router-dom';
 
 const Introduce =()=>{
@@ -9,26 +9,27 @@ const Introduce =()=>{
         console.log( "input 태그 동작")
     }
     function handleButton(){
-        navigate('/Product');
+        navigate('/product');
      }
     return(
-    
-        <div className='Bigbox'>
-            <div className='Myimg'>
+     <div className='center'>
+        <div className='bigbox'>
+            <div className='myimg'>
                  <img src='자기소개.jpeg' />  
             </div>
             <div className='Name'>
                 <h1>이청원</h1> 
-                <p className='Cardinal'>49기</p>
+                <p className='cardinal'>49기</p>
             </div>
-            <div className='Input'>
+            <div className='input'>
                 <input onChange={handleInput} type='text'></input>
                 <button onClick={handleButton}>클릭</button>
             </div>
-            <Link to='/Product'>회원가입</Link>;
+            <Link to='/product'>product 페에지로 넘어가기</Link>;
+        </div>
         </div>
     
     )
 }
-
+// 라우터.js 는 무조건적으로 인덱스.js랑 같이있어야한다.
 export default Introduce
