35fe9e37ff2a39af8f912a1a684fc8c463442fc7
jyj <dudwns5013@naver.com>
2023-09-03 22:05:40 +0900

    Assignment3-1 : Color 컴포넌트 구현Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Introduce from "./pages/Introduce/Introduce";
 import Product from "./pages/Product/Product";
+import Color from "./pages/Product/components/Color/Color";
 
 const Router = () => {
   return (
@@ -8,6 +9,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Introduce />} />
         <Route path="/product" element={<Product />} />
+        <Route path="/color" element={<Color />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Product/components/Color/Color.js
+++ src/pages/Product/components/Color/Color.js
@@ -1,18 +1,38 @@
 import React from "react";
+import { useState } from "react";
 import "./Color.scss";
 
 const Color = () => {
+  const [color, setColor] = useState("white");
+  const colorHandlerButton = (e) => {
+    setColor(e);
+  };
   return (
     <div className="color">
       <span className="colorText">
         색상 :
-        <div className="selected" />
-        white
+        <div className={`selected ${color}`} />
+        {color}
       </span>
       <div className="colorHandler">
-        <button className="colorButton white" onClick={() => {}} />
-        <button className="colorButton red" onClick={() => {}} />
-        <button className="colorButton yellow" onClick={() => {}} />
+        <button
+          className="colorButton white"
+          onClick={() => {
+            colorHandlerButton("white");
+          }}
+        />
+        <button
+          className="colorButton red"
+          onClick={() => {
+            colorHandlerButton("red");
+          }}
+        />
+        <button
+          className="colorButton yellow"
+          onClick={() => {
+            colorHandlerButton("yellow");
+          }}
+        />
       </div>
     </div>
   );
