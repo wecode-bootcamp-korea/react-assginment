688271d903921303f8521302bdec42207744ad32
jyj <dudwns5013@naver.com>
2023-09-04 01:19:40 +0900

    Assignment3-2 : Count 컴포넌트 구현

--- src/pages/Product/components/Count/Count.js
+++ src/pages/Product/components/Count/Count.js
@@ -1,15 +1,45 @@
 import React from "react";
+import { useState } from "react";
 import "./Count.scss";
 
 const Count = () => {
+  const [count, setCount] = useState(1);
+  const handleIncreaseBtn = () => {
+    setCount(count + 1);
+  };
+  const handleDecreaseBtn = () => {
+    if (count - 1) {
+      setCount(count - 1);
+    }
+  };
+  const handleResetBtn = () => {
+    setCount(1);
+  };
   return (
     <div className="count">
       <div className="countInput">
-        <button onClick={() => {}}>-</button>
-        <div className="countInputText">1</div>
-        <button onClick={() => {}}>+</button>
+        <button
+          onClick={() => {
+            handleDecreaseBtn();
+          }}
+        >
+          -
+        </button>
+        <div className="countInputText">{count}</div>
+        <button
+          onClick={() => {
+            handleIncreaseBtn();
+          }}
+        >
+          +
+        </button>
       </div>
-      <button className="resetBtn" onClick={() => {}}>
+      <button
+        className="resetBtn"
+        onClick={() => {
+          handleResetBtn();
+        }}
+      >
         reset
       </button>
     </div>
