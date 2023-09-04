1d29a0ee4522ed0e45b26bcd25b24d55ff0e0cd2
이영은 <hisenselee12@gmail.com>
2023-09-04 20:47:12 +0900

    [Assignment 3-1] React State : Color 컴포넌트 구현하기 완료

--- src/Router.js
+++ src/Router.js
@@ -2,15 +2,16 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Introduce from './pages/Introduce/Introduce';
 import Product from './pages/Product/Product';
+import Color from './pages/Product/components/Color/Color';
 
 const Router=()=>{
     return(
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Introduce/>} />
-                <Route path='/Introduce' element={<Introduce/>} />
-                <Route path='/Product' element={<Product/>} />
-
+                <Route path='/introduce' element={<Introduce/>} />
+                <Route path='/product' element={<Product/>} />
+                <Route path='/color' element={<Color/>} />
             </Routes>
         </BrowserRouter>
     );
