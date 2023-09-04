20a99473d41b58f004953c5d616ff291e4660e3b
sseyeon2 <sseyoun2@gmail.com>
2023-09-05 02:38:32 +0900

    Assignment3-3 : Review 컴포넌트 구현

--- src/Router.js
+++ src/Router.js
@@ -5,6 +5,7 @@ import Product from "./pages/Product/Product";
 import Color from "./pages/Product/components/Color/Color";
 import Count from "./pages/Product/components/Count/Count";
 import ProductList from "./pages/ProductList/ProductList";
+import Review from "./pages/Product/components/Review/Review";
 
 const Router = () => {
     return (
@@ -15,6 +16,7 @@ const Router = () => {
                 <Route path="/Color" element={<Color />} />
                 <Route path="/Count" element={<Count />} />
                 <Route path="/ProductList" element={<ProductList />} />
+                <Route path="/Review" element={<Review />} />
             </Routes>
         </BrowserRouter>
     );
