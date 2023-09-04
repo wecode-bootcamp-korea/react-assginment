653d60a46f83dbb59e11e4e8f3f0be13e60a0844
coolllleooo <buseness200824@gmail.com>
2023-09-04 17:09:24 +0900

    [Assignment 3-1] React State : Color 컴포넌트 구현하기

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Introduce from './pages/Introduce/Introduce'
-import Router from "./Router";
+//import Introduce from './pages/Introduce/Introduce'
+import Color from './pages/Product/components/Color/Color'
+//import Router from "./Router";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Introduce />);
-root.render(<Router />);
+//root.render(<Introduce />);
+root.render(<Color />);
+//root.render(<Router />);
--- src/pages/Product/components/Color/Color.js
+++ src/pages/Product/components/Color/Color.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Color.scss";
 
+
 const Color = () => {
+  const [color, setColor] = useState("white");
+  const [color2, setColor2] = useState("selected white");
+
+
   return (
     <div className="color">
       <span className="colorText">
         색상 :
-        <div className="selected" />
-        white
+        <div className={color2} />
+        {color}
       </span>
       <div className="colorHandler">
-        <button className="colorButton white" onClick={() => {}} />
-        <button className="colorButton red" onClick={() => {}} />
-        <button className="colorButton yellow" onClick={() => {}} />
+        <button className="colorButton white" onClick={() => {
+          setColor("white");
+          setColor2("selected white");
+          
+           }} />
+        <button className="colorButton red" onClick={() => {
+           setColor("red");
+           setColor2("selected red");
+
+        }} />
+        <button className="colorButton yellow" onClick={() => {
+           setColor("yellow");
+           setColor2("selected yellow");
+          
+        }} />
       </div>
     </div>
   );
