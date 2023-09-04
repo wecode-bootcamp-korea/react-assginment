7377da243211e95eb63d969b352ed079d3761d4c
sseyeon2 <sseyoun2@gmail.com>
2023-09-05 00:32:48 +0900

    Assignment3-2 : Count 컴포넌트 구현

--- src/pages/Product/components/Count/Count.js
+++ src/pages/Product/components/Count/Count.js
@@ -1,19 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Count.scss";
 
 const Count = () => {
-  return (
-    <div className="count">
-      <div className="countInput">
-        <button onClick={() => {}}>-</button>
-        <div className="countInputText">1</div>
-        <button onClick={() => {}}>+</button>
-      </div>
-      <button className="resetBtn" onClick={() => {}}>
-        reset
-      </button>
-    </div>
-  );
+    const [countNum, setCountNum] = useState(1);
+    const handlePlusButton = () => {
+        setCountNum(countNum + 1);
+    };
+    const handleMinusButton = () => {
+        if (countNum <= 1) {
+            return;
+        } else {
+            setCountNum(countNum - 1);
+        }
+    };
+
+    const resetButton = () => {
+        setCountNum(1);
+    };
+
+    return (
+        <div className="count">
+            <div className="countInput">
+                <button
+                    onClick={() => {
+                        handleMinusButton(countNum);
+                    }}
+                >
+                    -
+                </button>
+                <div className="countInputText">{countNum}</div>
+                <button
+                    onClick={() => {
+                        handlePlusButton(countNum);
+                    }}
+                >
+                    +
+                </button>
+            </div>
+            <button
+                className="resetBtn"
+                onClick={() => {
+                    resetButton();
+                    console.log(resetButton());
+                }}
+            >
+                reset
+            </button>
+        </div>
+    );
 };
 
 export default Count;
