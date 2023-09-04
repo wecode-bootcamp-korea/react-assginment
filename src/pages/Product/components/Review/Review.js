b7c31f98ef9fc7d746d3562b32e8ace1c3c97b5b
philamones <philamones@icloud.com>
2023-09-04 18:46:28 +0900

    Assignment3-3 : Review 컴포넌트 구현 완료

--- src/pages/Product/components/Review/Review.js
+++ src/pages/Product/components/Review/Review.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Review.scss";
 
 const Review = () => {
+  const [review, setReview] = useState(true);
+  const [text, setText] = useState("접기 ▲");
+
+  const reviewToggle = (review) => {
+    if (!review) {
+      setReview(true);
+      setText("접기 ▲");
+    } else {
+      setReview(false);
+      setText("더보기 ▼");
+    }
+  }
+
   return (
     <div className="review">
       <div className="user">
@@ -13,13 +26,13 @@ const Review = () => {
       </div>
       <div className="reviewTitle">배송도 빠르고 정말 좋아요.</div>
       <div className="reviewMain">
-        <div className="reviewMainContents">
+        <div className={`reviewMainContents ${review}`}>
           주문한지 하루만에 배송이 도착해서 너무 좋았습니다.
           <br />
           다음에도 필요하면 또 여기서 시킬 것 같아요.
         </div>
-        <span className="reviewMainHandler" onClick={() => {}}>
-          더보기 ▼
+        <span className="reviewMainHandler" onClick={() => {reviewToggle(review)}}>
+          {text}
         </span>
       </div>
     </div>
