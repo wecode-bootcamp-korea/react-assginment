a0bf7e517e59caf9516ced374e228363a71a2493
박요진 <syon013@naver.com>
2023-09-04 22:34:58 +0900

    [49기 박요진] Assignment3-3 : Review 컴포넌트 구현

--- src/pages/Product/components/Review/Review.js
+++ src/pages/Product/components/Review/Review.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Review.scss";
 
+const ReviewMain = () => {
+  return(
+  <div className="reviewMainContents">
+    주문한지 하루만에 배송이 도착해서 너무 좋았습니다.
+    <br />
+    다음에도 필요하면 또 여기서 시킬 것 같아요.
+  </div>
+  )
+}
+
 const Review = () => {
+
+  const [reviewOpen, setRiviewOpen] = useState(false);
+  const [more, setMore] = useState(false);
+
+
+  console.log(reviewOpen);
+
   return (
     <div className="review">
       <div className="user">
@@ -13,13 +30,15 @@ const Review = () => {
       </div>
       <div className="reviewTitle">배송도 빠르고 정말 좋아요.</div>
       <div className="reviewMain">
-        <div className="reviewMainContents">
-          주문한지 하루만에 배송이 도착해서 너무 좋았습니다.
-          <br />
-          다음에도 필요하면 또 여기서 시킬 것 같아요.
-        </div>
-        <span className="reviewMainHandler" onClick={() => {}}>
-          더보기 ▼
+        {reviewOpen === true ? <ReviewMain /> : null}
+        <span className="reviewMainHandler" onClick={() => {
+           if(reviewOpen === false) {
+            setRiviewOpen(true);
+          } else {
+            setRiviewOpen(false);
+          }
+        }}>
+          { reviewOpen === true ? '접기 ▲' : '더보기 ▼'}
         </span>
       </div>
     </div>
