2507157d7cba93593ea33c31d861aa9e21c8d4f1
coolllleooo <buseness200824@gmail.com>
2023-09-05 15:11:28 +0900

    Assignment3-3 : Review 컴포넌트 구현

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 //import Introduce from './pages/Introduce/Introduce'
-import Count from './pages/Product/components/Count/Count'
+//import Count from './pages/Product/components/Count/Count'
 //import Color from './pages/Product/components/Color/Color'
 //import Router from "./Router";
+import Review from './pages/Product/components/Review/Review'
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 //root.render(<Introduce />);
-root.render(<Count />);
+root.render(<Review />);
 //root.render(<Router />);
