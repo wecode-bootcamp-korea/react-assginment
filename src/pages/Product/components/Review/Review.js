c7ab2d1f3c9c68e491e5101eac52c21c5301fa81
박인국 <pkook7@gmail.com>
2023-09-05 10:31:37 +0900

    Assignment3-3 : Review 컴포넌트 구현

--- src/pages/Product/components/Review/Review.js
+++ src/pages/Product/components/Review/Review.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Review.scss";
 
 const Review = () => {
+  const [moreBox, setMoreBox] = useState('none');
+  const [moreBtn, setMoreBtn] = useState('open');
+  const moreBtnFunction = () => {
+      if(moreBtn==='open'){
+        document.querySelector('.reviewMainHandler').innerText='접기 ▲'
+        setMoreBox('block');
+        setMoreBtn('close');
+      } else if (moreBtn==='close'){
+        document.querySelector('.reviewMainHandler').innerText='더보기 ▼'
+        setMoreBox('none');
+        setMoreBtn('open');
+    }
+  }
   return (
     <div className="review">
       <div className="user">
@@ -13,12 +26,12 @@ const Review = () => {
       </div>
       <div className="reviewTitle">배송도 빠르고 정말 좋아요.</div>
       <div className="reviewMain">
-        <div className="reviewMainContents">
+        <div className="reviewMainContents" style={{display:moreBox}}>
           주문한지 하루만에 배송이 도착해서 너무 좋았습니다.
           <br />
           다음에도 필요하면 또 여기서 시킬 것 같아요.
         </div>
-        <span className="reviewMainHandler" onClick={() => {}}>
+        <span className="reviewMainHandler" onClick={moreBtnFunction}>
           더보기 ▼
         </span>
       </div>
